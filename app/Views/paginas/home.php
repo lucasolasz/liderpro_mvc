1512a9159bd00d06c200f93629f551f6f74195d0
lucasolasz <olaszlucas072@gmail.com>
2023-03-14 23:43:33 -0300

    Inclui gifs da home

--- app/Views/paginas/home.php
+++ app/Views/paginas/home.php
@@ -1,5 +1,54 @@
-<div class="container p-5">
+<div class="container py-5">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?= URL . '/img/banner/banner_gestao_informatica.png' ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= URL . '/img/banner/banner_infra_rede.png' ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= URL . '/img/banner/banner_nobreak.png' ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= URL . '/img/banner/banner_solucao_nuvem.png' ?>" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </button>
+    </div>
 
-   
+
+    <div class="py-5 d-flex align-items-center justify-content-center lp-home-gifs">
+        <div class="col-md">
+            <h6>NOSSOS NÚMEROS</h6>
+            <img class="img-fluid" src="<?= URL . '/img/gifs_home/nossos_numeros.gif' ?>" alt="">
+        </div>
+        <div class="col-md">
+            <h6>PARCERIAS E CERTIFICAÇÕES</h6>
+            <img class="img-fluid" src="<?= URL . '/img/gifs_home/parcerias.gif' ?>" alt="">
+        </div>
+        <div class="col-md">
+            <h6>SERVIÇOS ESPECIAIS</h6>
+            <img class="img-fluid" src="<?= URL . '/img/gifs_home/servicos_especiais.gif' ?>" alt="">
+        </div>
+        <div class="col-md">
+            <h6>DEPOIMENTOS</h6>
+            <img class="img-fluid" src="<?= URL . '/img/gifs_home/depoimentos.gif' ?>" alt="">
+        </div>
+    </div>
+
 
 </div>
