cf101ed40bdf9e3e99796e3da3ecadb17a184046
lucasolasz <olaszlucas072@gmail.com>
2023-03-16 21:56:56 -0300

    Implementa tela Gestao de informática

--- app/Views/paginas/gestaoInformatica.php
+++ app/Views/paginas/gestaoInformatica.php
@@ -1,5 +1,158 @@
 <div class="container p-5">
     <div class="row">
-        
+        <img class="img-fluid" src="<?= URL . '/img/banner/banner_gestao_informatica.png' ?>" alt="">
+    </div>
+    <div class="row py-5 d-flex align-items-center">
+        <div class="col-md">
+            <img class="img-fluid" src="<?= URL . '/img/perguntas/gestao_ti_pergunta1.png' ?>" alt="">
+        </div>
+        <div class="col-md py-5">
+            <img class="img-fluid" src="<?= URL . '/img/perguntas/gestao_ti_pergunta2.png' ?>" alt="">
+        </div>
+        <div class="col-md">
+            <img class="img-fluid" src="<?= URL . '/img/perguntas/gestao_ti_pergunta3.png' ?>" alt="">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-9 col-lg-9 d-flex align-items-center">
+            <p class="lp-paragrafo "> Nosso modelo de Gestão de TI abrange, de forma integrada, toda a estrutura de tecnologia que pode ser monitorada em até 24h x 7 dias e que se adequa a cada cliente conforme a sua dimensão e a criticidade das suas operações. Otimizando, assim, todos os serviços terceirizados de informática, telefonia, segurança eletrônica e energia. De forma organizada e dinâmica, auditamos toda a demanda de tecnologia -ambiente e equipamentos- da empresa e apresentamos um relatório com as sugestões apropriadas a uma melhor performance e segurança na área de TI. <a class="ml-2 lp-link-veja-mais" href="#">VEJA +</a>
+            </p>
+        </div>
+        <div class="col-md-3 col-lg-3 d-flex justify-content-center">
+            <img class="img-fluid" style="height: 200px; width: 180px" src="<?= URL . '/img/fluxo_gestao_informatica.png' ?>" alt="">
+        </div>
+    </div>
+
+    <div class="row py-5">
+
+
+        <!-- <div class="col-sm-1 col-md-1 col-lg-1 lp-tab-servicos">SERVIÇOS</div> -->
+
+        <div class="col-sm-11 col-md-11 col-lg-11">
+
+            <!-- Tab links -->
+            <div class="tab-lp">
+                <button class="tablinks" onclick="openCity(event, 'Fotos')" id="defaultOpen">FOTOS</button>
+                <button class="tablinks" onclick="openCity(event, 'informática')">INFORMÁTICA</button>
+                <button class="tablinks" onclick="openCity(event, 'telefonia')">TELEFONIA</button>
+                <button class="tablinks" onclick="openCity(event, 'segurancaEletronica')">SEGURANÇA ELETRÔNICA</button>
+                <button class="tablinks" onclick="openCity(event, 'eletricaEArCondicionado')">ELÉTRICA E AR-CONDICIONADO</button>
+            </div>
+            <!-- Tab content -->
+            <div id="Fotos" class="tabcontent-lp">
+                <h1>FOTOS</h1>
+            </div>
+
+            <div id="informática" class="tabcontent-lp">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Servidores e Nuvem - Microsoft, Apple e Linux</li>
+                            <li>Ativos de Rede - Roteador, Modem, Firewall</li>
+                            <li>Segurança da Informação, Antivírus e Backup</li>
+                            <li>Suporte a usuários local e remotamente</li>
+                        </ol>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Infraestrutura de Rede - Cabeamento</li>
+                            <li>Sistemas de Conferência de Áudio e Vídeo</li>
+                            <li>Reparo eletrônico de diversos tipos de equipamentos</li>
+                            <li>Inventário de equipamentos ativos de informática</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div id="telefonia" class="tabcontent-lp">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Sistema de Telefonia IP PBX e PABX Digital Algumas Características:</li>
+                            <li>URA - Unidade de Resposta Audível</li>
+                            <li>DAC - Distribuidor Automático de Chamadas </li>
+                            <!-- <li>Suporte a usuários local e remotamente</li> -->
+                        </ol>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Gravação das ligações telefônicas</li>
+                            <li>Capacidade ilimitada de ramais físicos e virtuais - celular</li>
+                            <li>Relatórios e banco de dados próprios e ilimitados</li>
+                            <li>Aparelhos com conferência de áudio e vídeo individuais</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div id="segurancaEletronica" class="tabcontent-lp">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Sistema de Câmeras Digitais e IPs</li>
+                            <li>Sistema de Alarmes</li>
+                            <li>Sistema de Controle de Acesso</li>
+                            <li>Sistema de Ponto Eletrônico</li>
+                        </ol>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Exibição de imagens em tempo real</li>
+                            <li>Gravação de imagens na nuvem</li>
+                            <li>Relatórios personalizados</li>
+                            <li>Banco de dados flexível para uso em diversos programas</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div id="eletricaEArCondicionado" class="tabcontent-lp">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Sistema de Proteção e Gerenciamento de Energia</li>
+                            <li>Sistema de Circuito Elétrico Estabilizado</li>
+                            <li>Instalação Elétrica em Baixa Tensão</li>
+                            <li>Reforma de Quadro de Distribuição de Energia</li>
+                        </ol>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Instalação de No-break, Estabilizador e Transformador</li>
+                            <li>Instalação de Ares-condicionados e Automação</li>
+                            <li>Automação de ares-condic. por tempo e/ou temperatura</li>
+                            <li>Contratos Flexíveis conforme o perfil do cliente</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
+<script>
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+
+    function openCity(evt, cityName) {
+        // Declare all variables
+        var i, tabcontent, tablinks;
+
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent-lp");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+</script>
