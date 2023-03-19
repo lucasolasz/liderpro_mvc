290e6b2128cb8fbbfda95b5db63105101d7c58d8
lucasolasz <olaszlucas072@gmail.com>
2023-03-19 13:56:46 -0300

    Implementa página Cabeamento Estruturado

--- app/Views/paginas/cabeamentoEstruturado.php
+++ app/Views/paginas/cabeamentoEstruturado.php
@@ -1,5 +1,135 @@
 <div class="container p-5">
     <div class="row">
-        <?php echo $dados['tituloBreadcrumb']; ?>
+        <img class="img-fluid" src="<?= URL . '/img/paginas/cabeamento_estruturado/banner_infra_rede.png' ?>" alt="">
+    </div>
+    <div class="row py-5 d-flex align-items-center">
+        <div class="col-md">
+            <img class="img-fluid" src="<?= URL . '/img/paginas/cabeamento_estruturado/cabeamento_estruturado_pergunta1.png' ?>" alt="">
+        </div>
+        <div class="col-md py-5">
+            <img class="img-fluid" src="<?= URL . '/img/paginas/cabeamento_estruturado/cabeamento_estruturado_pergunta2.png' ?>" alt="">
+        </div>
+        <div class="col-md">
+            <img class="img-fluid" src="<?= URL . '/img/paginas/cabeamento_estruturado/cabeamento_estruturado_pergunta3.png' ?>" alt="">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-9 col-lg-9 d-flex align-items-center">
+            <p class="lp-paragrafo "> Transformamos a tradicional estruturado de rede em uma solução de infraestrutura digital que engloba tecnologias inovadoras a atender novos recursos habilitados por hiperconvergência, virtualização e nuvem, tornando-a mais eficiente e flexível a demanda que estar por vir.
+                Desenvolvemos e executamos projetos de rede estruturada em cabeamento UTP nas categorias 5e, 6, 6A e 7, além de fibra óptica. Respeitando estritamente as normas nacional NBR 16.665 e internacionais ANSI/TIA/EIA/ISO/IEEE, que suportam diversas tecnologias, dispositivos, recursos e demanda, atendendo de grupo de trabalho aos maiores requisitos corporativos para tráfego de internet, dados, imagem e voz. <a class="ml-2 lp-link-veja-mais" href="#">VEJA +</a>
+            </p>
+        </div>
+        <div class="col-md-3 col-lg-3 d-flex justify-content-center">
+            <img class="img-fluid" style="height: 200px; width: 180px" src="<?= URL . '/img/paginas/cabeamento_estruturado/imagem_texto_cabeamento_estruturado.png' ?>" alt="">
+        </div>
+    </div>
+
+    <div class="row py-5">
+
+
+        <!-- <div class="col-sm-1 col-md-1 col-lg-1 lp-tab-servicos">SERVIÇOS</div> -->
+
+        <div class="col-sm-11 col-md-11 col-lg-11">
+
+            <h2 class="py-5">Serviços</h2>
+
+            <!-- Tab links -->
+            <div class="tab-lp">
+                <button class="tablinks" onclick="openCity(event, 'tab0')" id="defaultOpen">FOTOS</button>
+                <button class="tablinks" onclick="openCity(event, 'tab1')">REDE ESTRUTURADA</button>
+                <button class="tablinks" onclick="openCity(event, 'tab2')">ORGANIZAÇÃO E RACKS</button>
+                <button class="tablinks" onclick="openCity(event, 'tab3')">REDE DE SEM FIO</button>
+                <button class="tablinks" onclick="openCity(event, 'tab4')">ATIVOS DE REDE</button>
+            </div>
+            <!-- Tab content -->
+            <div id="tab0" class="tabcontent-lp">
+                <h1>FOTOS</h1>
+            </div>
+
+            <div id="tab1" class="tabcontent-lp">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Infraestrutura de Rede - Cabeamento Estruturado</li>
+                            <li>Expansão de Rede - Estrutura, Cabos e Pontos</li>
+                            <li>Organização de Rack - Equip. Ativos e Passivos</li>
+                            <li>Certificação de Cabeamento Metálico - UTP</li>
+                        </ol>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Instalação, Reparo e Fusão de Fibra Óptica</li>
+                            <li>Certificação de Fibra Óptica</li>
+                            <li>Teste de Performance e Perda Óptica</li>
+                            <li>Microcabos e Microfibras</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div id="tab2" class="tabcontent-lp">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Sistema de Telefonia IP PBX e PABX Digital Algumas Características:</li>
+                            <li>URA - Unidade de Resposta Audível</li>
+                            <li>DAC - Distribuidor Automático de Chamadas </li>
+                            <!-- <li>Suporte a usuários local e remotamente</li> -->
+                        </ol>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Gravação das ligações telefônicas</li>
+                            <li>Capacidade ilimitada de ramais físicos e virtuais - celular</li>
+                            <li>Relatórios e banco de dados próprios e ilimitados</li>
+                            <li>Aparelhos com conferência de áudio e vídeo individuais</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div id="tab3" class="tabcontent-lp">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Sistema de Câmeras Digitais e IPs</li>
+                            <li>Sistema de Alarmes</li>
+                            <li>Sistema de Controle de Acesso</li>
+                            <li>Sistema de Ponto Eletrônico</li>
+                        </ol>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Exibição de imagens em tempo real</li>
+                            <li>Gravação de imagens na nuvem</li>
+                            <li>Relatórios personalizados</li>
+                            <li>Banco de dados flexível para uso em diversos programas</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div id="tab4" class="tabcontent-lp">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Sistema de Proteção e Gerenciamento de Energia</li>
+                            <li>Sistema de Circuito Elétrico Estabilizado</li>
+                            <li>Instalação Elétrica em Baixa Tensão</li>
+                            <li>Reforma de Quadro de Distribuição de Energia</li>
+                        </ol>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ol class="lp-lista-tab">
+                            <li>Instalação de No-break, Estabilizador e Transformador</li>
+                            <li>Instalação de Ares-condicionados e Automação</li>
+                            <li>Automação de ares-condic. por tempo e/ou temperatura</li>
+                            <li>Contratos Flexíveis conforme o perfil do cliente</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
--- app/Views/paginas/gestaoInformatica.php
+++ app/Views/paginas/gestaoInformatica.php
@@ -31,6 +31,8 @@
 
         <div class="col-sm-11 col-md-11 col-lg-11">
 
+            <h2 class="py-5">Serviços</h2>
+
             <!-- Tab links -->
             <div class="tab-lp">
                 <button class="tablinks" onclick="openCity(event, 'Fotos')" id="defaultOpen">FOTOS</button>
@@ -130,29 +132,3 @@
         </div>
     </div>
 </div>
-
-<script>
-    // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
-
-    function openCity(evt, cityName) {
-        // Declare all variables
-        var i, tabcontent, tablinks;
-
-        // Get all elements with class="tabcontent" and hide them
-        tabcontent = document.getElementsByClassName("tabcontent-lp");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-
-        // Get all elements with class="tablinks" and remove the class "active"
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-
-        // Show the current tab, and add an "active" class to the button that opened the tab
-        document.getElementById(cityName).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
-</script>
