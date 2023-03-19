09accbefeba4fb1b2e0d486d92531ecfb75913b8
lucasolasz <olaszlucas072@gmail.com>
2023-03-19 13:21:10 -0300

    Inclui arquivo javascript ao projeto

--- app/Views/topo.php
+++ app/Views/topo.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-
+    <script src="<?php echo URL ?>/public/js/jquery.funcoes.js"></script>
 
 
     <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
