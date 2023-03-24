11b7d5c986be6d06e055246e2d41a99fd048d498
lucasolasz <olaszlucas072@gmail.com>
2023-03-23 23:26:01 -0300

    Insere Links no rodapé

--- app/Views/rodape.php
+++ app/Views/rodape.php
@@ -1,10 +1,10 @@
 <footer class="lp-rodape d-flex justify-content-center p-5">
 
-    <div class="row container">        
+    <div class="row container">
         <div class="col-md">
-            <ul class="list-unstyled">
+            <ul class="list-unstyled lista-rodape">
                 <li class="lp-titulo-rodape">SERVIÇOS</li>
-                <li>ASSISTÊNCIA TÉCNICA
+                <li><a href="<?= URL . '/Paginas/assistencia_tecnica' ?>">ASSISTÊNCIA TÉCNICA</a>
                     <ul class="list-unstyled lp-submenu-rodape">
                         <li>Computadores</li>
                         <li>Notebook</li>
@@ -16,32 +16,33 @@
                         <li>No-break</li>
                     </ul>
                 </li>
-                <li>GESTÃO DE INFORMÁTICA</li>
-                <li>CABEAMENTO ESTRUTURADO</li>
-                <li>SEGURANÇA ELETRÔNICA</li>
-                <li>SISTEMAS DE TELEFONIA</li>
-                <li>SOLUÇÕES EM NUVEM</li>
+                <li><a href="<?= URL . '/Paginas/gestao_informatica' ?>">GESTÃO DE INFORMÁTICA</a> </li>
+                <li><a href="<?= URL . '/Paginas/cabeamento_estruturado' ?>">CABEAMENTO ESTRUTURADO</a></li>
+                <li><a href="#">SEGURANÇA ELETRÔNICA</a></li>
+                <li><a href="#">SISTEMAS DE TELEFONIA</a></li>
+                <li><a href="<?= URL . '/Paginas/solucoes_nobreak' ?>">SOLUÇÕES DE NOBREAKS</a></li>
+                <li><a href="<?= URL . '/Paginas/solucoes_em_nuvem' ?>"> SOLUÇÕES EM NUVEM</a></li>
             </ul>
         </div>
         <div class="col-md">
-            <ul class="list-unstyled">
+            <ul class="list-unstyled lista-rodape">
                 <li class="lp-titulo-rodape">LIDERPRO</li>
-                <li>VALORES
+                <li><a href="<?= URL . '/LiderPro/valores' ?>">VALORES</a>
                     <ul class="list-unstyled lp-submenu-rodape">
-                        <li>Nossa Missão</li>
-                        <li>Nossa Visão</li>
-                        <li>Honestidade e Confiança</li>
-                        <li>Respeito e Lei</li>
-                        <li>Integridade</li>
-                        <li>Direitos Humanos</li>
+                        <li><a href="<?= URL . '/LiderPro/valores' ?>">Nossa Missão</a> </li>
+                        <li><a href="<?= URL . '/LiderPro/valores' ?>">Nossa Visão</a></li>
+                        <li><a href="<?= URL . '/LiderPro/valores' ?>">Honestidade e Confiança</a></li>
+                        <li><a href="<?= URL . '/LiderPro/valores' ?>">Respeito e Lei</a></li>
+                        <li><a href="<?= URL . '/LiderPro/valores' ?>">Integridade</a></li>
+                        <li><a href="<?= URL . '/LiderPro/valores' ?>">Direitos Humanos</a></li>
                     </ul>
                 </li>
-                <li>PARCEIROS</li>
-                <li>INFORMAÇÕES
+                <li><a href="<?= URL . '/LiderPro/parcerias' ?>">PARCEIROS</a></li>
+                <li><a href="<?= URL . '/LiderPro/informacoes_legais' ?>">INFORMAÇÕES</a> 
                     <ul class="list-unstyled lp-submenu-rodape">
-                        <li>Termos de Uso</li>
-                        <li>Política de Privacidade</li>
-                        <li>Informações Legais</li>
+                        <li><a href="<?= URL . '/LiderPro/termos_de_uso' ?>">Termos de Uso</a></li>
+                        <li><a href="<?= URL . '/LiderPro/politica_de_privacidade' ?>">Política de Privacidade</a></li>
+                        <li><a href="<?= URL . '/LiderPro/informacoes_legais' ?>">Informações Legais</a></li>
                     </ul>
                 </li>
                 <li>ACESSO AO CLIENTE</li>
