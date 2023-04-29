aa9232fb2efdbdcdc52cde960d94e133e2c9c102
lucasolasz <olaszlucas072@gmail.com>
2023-04-28 23:05:30 -0300

    Remove caracteres especiais da string

--- app/Helpers/RemoveAcentosString.php
+++ app/Helpers/RemoveAcentosString.php
@@ -2,17 +2,21 @@
 
 class RemoveAcentosString
 {
-    public static function removeAcentoEDeixaMinusculaString($string){
+    public static function removeAcentoEDeixaMinusculaString($string)
+    {
 
-        $string = mb_strtolower($string, 'UTF-8'); // converte para minúsculas
+        // Remove acentos
+        $string = mb_strtolower($string, 'UTF-8');
         $string = preg_replace('/[áàãâä]/ui', 'a', $string);
         $string = preg_replace('/[éèêë]/ui', 'e', $string);
         $string = preg_replace('/[íìîï]/ui', 'i', $string);
         $string = preg_replace('/[óòôõö]/ui', 'o', $string);
         $string = preg_replace('/[úùûü]/ui', 'u', $string);
         $string = preg_replace('/[ç]/ui', 'c', $string);
-        
+
+        // Remove caracteres especiais
+        $string = preg_replace('/[^a-z0-9\s]/i', '', $string);
+
         return $string;
     }
-
 }
