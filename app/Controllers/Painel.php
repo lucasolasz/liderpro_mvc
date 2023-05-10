d52310f2f830ef438f9dfd3381eb8faa0b2802a2
conuaz <lucas.olasz@tty2000.com.br>
2023-05-10 14:44:15 -0300

    Atualiza campos descritivos das páginas.

--- app/Controllers/Painel.php
+++ app/Controllers/Painel.php
@@ -142,6 +142,66 @@ class Painel extends Controller
 
         $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         if (isset($formulario)) {
+
+            $dados = [
+                'id_pagina' => $id,
+                'txtTitutoPagina' => trim($formulario['txtTitutoPagina']),
+                'txtTextPrincipal' => trim($formulario['txtTextPrincipal']),
+                'txtNomeTab1' => trim($formulario['txtNomeTab1']),
+                'txtTopico1Tab1' => trim($formulario['txtTopico1Tab1']),
+                'txtTopico2Tab1' => trim($formulario['txtTopico2Tab1']),
+                'txtTopico3Tab1' => trim($formulario['txtTopico3Tab1']),
+                'txtTopico4Tab1' => trim($formulario['txtTopico4Tab1']),
+                'txtTopico5Tab1' => trim($formulario['txtTopico5Tab1']),
+                'txtTopico6Tab1' => trim($formulario['txtTopico6Tab1']),
+                'txtTopico7Tab1' => trim($formulario['txtTopico7Tab1']),
+                'txtTopico8Tab1' => trim($formulario['txtTopico8Tab1']),
+                'txtNomeTab2' => trim($formulario['txtNomeTab2']),
+                'txtTopico1Tab2' => trim($formulario['txtTopico1Tab2']),
+                'txtTopico2Tab2' => trim($formulario['txtTopico2Tab2']),
+                'txtTopico3Tab2' => trim($formulario['txtTopico3Tab2']),
+                'txtTopico4Tab2' => trim($formulario['txtTopico4Tab2']),
+                'txtTopico5Tab2' => trim($formulario['txtTopico5Tab2']),
+                'txtTopico6Tab2' => trim($formulario['txtTopico6Tab2']),
+                'txtTopico7Tab2' => trim($formulario['txtTopico7Tab2']),
+                'txtTopico8Tab2' => trim($formulario['txtTopico8Tab2']),
+                'txtNomeTab3' => trim($formulario['txtNomeTab3']),
+                'txtTopico1Tab3' => trim($formulario['txtTopico1Tab3']),
+                'txtTopico2Tab3' => trim($formulario['txtTopico2Tab3']),
+                'txtTopico3Tab3' => trim($formulario['txtTopico3Tab3']),
+                'txtTopico4Tab3' => trim($formulario['txtTopico4Tab3']),
+                'txtTopico5Tab3' => trim($formulario['txtTopico5Tab3']),
+                'txtTopico6Tab3' => trim($formulario['txtTopico6Tab3']),
+                'txtTopico7Tab3' => trim($formulario['txtTopico7Tab3']),
+                'txtTopico8Tab3' => trim($formulario['txtTopico8Tab3']),
+                'txtNomeTab4' => trim($formulario['txtNomeTab4']),
+                'txtTopico1Tab4' => trim($formulario['txtTopico1Tab4']),
+                'txtTopico2Tab4' => trim($formulario['txtTopico2Tab4']),
+                'txtTopico3Tab4' => trim($formulario['txtTopico3Tab4']),
+                'txtTopico4Tab4' => trim($formulario['txtTopico4Tab4']),
+                'txtTopico5Tab4' => trim($formulario['txtTopico5Tab4']),
+                'txtTopico6Tab4' => trim($formulario['txtTopico6Tab4']),
+                'txtTopico7Tab4' => trim($formulario['txtTopico7Tab4']),
+                'txtTopico8Tab4' => trim($formulario['txtTopico8Tab4']),
+                'chkPaginaAtiva' => $formulario['chkPaginaAtiva']
+
+            ];
+
+            // $dados['id_pagina'] = $id;
+
+            // $dados['fileBannerPrincipal'] = isset($_FILES['fileBannerPrincipal']) ? $_FILES['fileBannerPrincipal'] : "";
+            // $dados['filePerguntas'] = isset($_FILES['filePerguntas']) ? $_FILES['filePerguntas'] : "";
+            // $dados['fileFotoTexto'] = isset($_FILES['fileFotoTexto']) ? $_FILES['fileFotoTexto'] : "";
+            // $dados['fileFotosServico'] = isset($_FILES['fileFotosServico']) ? $_FILES['fileFotosServico'] : "";
+
+            if ($this->paginasModel->atualizarPagina($dados)) {
+                //Para exibir mensagem success , não precisa informar o tipo de classe
+                Alertas::mensagem('paginas', 'Página atualizada com sucesso');
+                Redirecionamento::redirecionar('Painel/visualizarPaginas');
+            } else {
+                Alertas::mensagem('paginas', 'Não foi possível atualizar a página', 'alert alert-danger');
+                Redirecionamento::redirecionar('Painel/visualizarPaginas');
+            }
         } else {
             $dados = [
                 'paginas' => $paginas,
--- app/Models/Pagina.php
+++ app/Models/Pagina.php
@@ -155,7 +155,7 @@ class Pagina
                 :ds_topico8tab4,
                 :chk_pagina_ativa
                 )");
-                
+
         $ds_breadcrumb_menu  = GeraNomeArquivoComUnderline::geraNomeArquivoComUnderline($dados['txtTitutoPagina']);
 
         $this->db->bind("ds_pagina", trim($dados['txtTitutoPagina']));
@@ -304,5 +304,99 @@ class Pagina
 
             $this->armazenaImagemDinamicamente("tb_foto_pergunta", $pastaArquivo, $novoNomeArquivoAposMover, $ultimoId);
         }
+    }
+
+    public function atualizarPagina($dados)
+    {
+
+        $this->db->query("UPDATE tb_pagina SET 
+        ds_pagina=:ds_pagina,
+        ds_texto_principal=:ds_texto_principal, 
+        ds_tab1=:ds_tab1, 
+        ds_topico1tab1=:ds_topico1tab1, 
+        ds_topico2tab1=:ds_topico2tab1, 
+        ds_topico3tab1=:ds_topico3tab1, 
+        ds_topico4tab1=:ds_topico4tab1, 
+        ds_topico5tab1=:ds_topico5tab1, 
+        ds_topico6tab1=:ds_topico6tab1, 
+        ds_topico7tab1=:ds_topico7tab1, 
+        ds_topico8tab1=:ds_topico8tab1, 
+        ds_tab2=:ds_tab2,
+        ds_topico1tab2=:ds_topico1tab2, 
+        ds_topico2tab2=:ds_topico2tab2, 
+        ds_topico3tab2=:ds_topico3tab2, 
+        ds_topico4tab2=:ds_topico4tab2, 
+        ds_topico5tab2=:ds_topico5tab2, 
+        ds_topico6tab2=:ds_topico6tab2, 
+        ds_topico7tab2=:ds_topico7tab2, 
+        ds_topico8tab2=:ds_topico8tab2, 
+        ds_tab3=:ds_tab3, 
+        ds_topico1tab3=:ds_topico1tab3, 
+        ds_topico2tab3=:ds_topico2tab3, 
+        ds_topico3tab3=:ds_topico3tab3, 
+        ds_topico4tab3=:ds_topico4tab3, 
+        ds_topico5tab3=:ds_topico5tab3, 
+        ds_topico6tab3=:ds_topico6tab3, 
+        ds_topico7tab3=:ds_topico7tab3, 
+        ds_topico8tab3=:ds_topico8tab3, 
+        ds_tab4=:ds_tab4, 
+        ds_topico1tab4=:ds_topico1tab4, 
+        ds_topico2tab4=:ds_topico2tab4, 
+        ds_topico3tab4=:ds_topico3tab4, 
+        ds_topico4tab4=:ds_topico4tab4, 
+        ds_topico5tab4=:ds_topico5tab4, 
+        ds_topico6tab4=:ds_topico6tab4, 
+        ds_topico7tab4=:ds_topico7tab4, 
+        ds_topico8tab4=:ds_topico8tab4, 
+        chk_pagina_ativa=:chk_pagina_ativa
+        
+        WHERE id_pagina= :id_pagina;
+        ");
+
+        $this->db->bind("ds_pagina", trim($dados['txtTitutoPagina']));
+        $this->db->bind("ds_texto_principal", $dados['txtTextPrincipal']);
+        $this->db->bind("ds_tab1", $dados['txtNomeTab1']);
+        $this->db->bind("ds_topico1tab1", $dados['txtTopico1Tab1']);
+        $this->db->bind("ds_topico2tab1", $dados['txtTopico2Tab1']);
+        $this->db->bind("ds_topico3tab1", $dados['txtTopico3Tab1']);
+        $this->db->bind("ds_topico4tab1", $dados['txtTopico4Tab1']);
+        $this->db->bind("ds_topico5tab1", $dados['txtTopico5Tab1']);
+        $this->db->bind("ds_topico6tab1", $dados['txtTopico6Tab1']);
+        $this->db->bind("ds_topico7tab1", $dados['txtTopico7Tab1']);
+        $this->db->bind("ds_topico8tab1", $dados['txtTopico8Tab1']);
+        $this->db->bind("ds_tab2", $dados['txtNomeTab2']);
+        $this->db->bind("ds_topico1tab2", $dados['txtTopico1Tab2']);
+        $this->db->bind("ds_topico2tab2", $dados['txtTopico2Tab2']);
+        $this->db->bind("ds_topico3tab2", $dados['txtTopico3Tab2']);
+        $this->db->bind("ds_topico4tab2", $dados['txtTopico4Tab2']);
+        $this->db->bind("ds_topico5tab2", $dados['txtTopico5Tab2']);
+        $this->db->bind("ds_topico6tab2", $dados['txtTopico6Tab2']);
+        $this->db->bind("ds_topico7tab2", $dados['txtTopico7Tab2']);
+        $this->db->bind("ds_topico8tab2", $dados['txtTopico8Tab2']);
+        $this->db->bind("ds_tab3", $dados['txtNomeTab3']);
+        $this->db->bind("ds_topico1tab3", $dados['txtTopico1Tab3']);
+        $this->db->bind("ds_topico2tab3", $dados['txtTopico2Tab3']);
+        $this->db->bind("ds_topico3tab3", $dados['txtTopico3Tab3']);
+        $this->db->bind("ds_topico4tab3", $dados['txtTopico4Tab3']);
+        $this->db->bind("ds_topico5tab3", $dados['txtTopico5Tab3']);
+        $this->db->bind("ds_topico6tab3", $dados['txtTopico6Tab3']);
+        $this->db->bind("ds_topico7tab3", $dados['txtTopico7Tab3']);
+        $this->db->bind("ds_topico8tab3", $dados['txtTopico8Tab3']);
+        $this->db->bind("ds_tab4", $dados['txtNomeTab4']);
+        $this->db->bind("ds_topico1tab4", $dados['txtTopico1Tab4']);
+        $this->db->bind("ds_topico2tab4", $dados['txtTopico2Tab4']);
+        $this->db->bind("ds_topico3tab4", $dados['txtTopico3Tab4']);
+        $this->db->bind("ds_topico4tab4", $dados['txtTopico4Tab4']);
+        $this->db->bind("ds_topico5tab4", $dados['txtTopico5Tab4']);
+        $this->db->bind("ds_topico6tab4", $dados['txtTopico6Tab4']);
+        $this->db->bind("ds_topico7tab4", $dados['txtTopico7Tab4']);
+        $this->db->bind("ds_topico8tab4", $dados['txtTopico8Tab4']);
+        $this->db->bind("chk_pagina_ativa", $dados['chkPaginaAtiva']);
+        $this->db->bind("id_pagina", $dados['id_pagina']);
+
+        $this->db->executa();
+        
+        return true;
+
     }
 }
