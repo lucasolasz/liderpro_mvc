1bf85bb2957a02cfb8b7e3d689b4569e6dc69040
conuaz <lucas.olasz@tty2000.com.br>
2023-05-12 11:21:18 -0300

    Controller padrão gerado dinamicamente

--- app/Controllers/PaginasDinamicas.php
+++ app/Controllers/PaginasDinamicas.php
@@ -7,9 +7,34 @@ class PaginasDinamicas extends Controller
     }
     
         
+    public function index() { 
+        $paginas = $this->paginaDinamicaModel->listarPaginasAtivas();
+
+        $dados = [
+            'paginas' => $paginas,
+            'tituloBreadcrumb' => 'HOME' ,
+        ];
+
+        $this->view('paginas/home', $dados);
+    }
+    
+        
+    public function assistencia_tecnica() { 
+        $paginas = $this->paginaDinamicaModel->listarPaginasAtivas();
+
+        $dados = [
+            'paginas' => $paginas,
+            'tituloBreadcrumb' => 'assistencia_tecnica' ,
+        ];
+
+        $this->view('paginas/assistencia_tecnica', $dados);
+    }
+    
+        
     public function gestao_informatica() { 
 
         $idPaginaSelecionada = 78;
+        $paginas = $this->paginaDinamicaModel->listarPaginasAtivas();
         $paginaSelecionada = $this->paginaDinamicaModel->listarPaginasPeloId($idPaginaSelecionada);
         $dolarFotoBanner = $this->paginaDinamicaModel->listarFotoBannerPeloId($idPaginaSelecionada);
         $dolarFotoPergunta = $this->paginaDinamicaModel->listarFotoPerguntaPeloId($idPaginaSelecionada);
@@ -17,7 +42,8 @@ class PaginasDinamicas extends Controller
         $dolarFotoTexto = $this->paginaDinamicaModel->listarFotoTextoPeloId($idPaginaSelecionada);
 
         $dados = [
-            'tituloBreadCrumb' => 'gestao_informatica' ,
+            'paginas' => $paginas,
+            'tituloBreadcrumb' => 'gestao_informatica' ,
             'paginaSelecionada' => $paginaSelecionada,
             'dolarFotoBanner' => $dolarFotoBanner,
             'dolarFotoPergunta' => $dolarFotoPergunta,
@@ -25,6 +51,75 @@ class PaginasDinamicas extends Controller
             'dolarFotoTexto' => $dolarFotoTexto
         ];
 
-        $this->view('paginas/templatePagina', $dados);
+        $this->view('paginas/gestao_informatica', $dados);
+    }
+        
+    public function cabeamento_estruturado() { 
+
+        $idPaginaSelecionada = 79;
+        $paginas = $this->paginaDinamicaModel->listarPaginasAtivas();
+        $paginaSelecionada = $this->paginaDinamicaModel->listarPaginasPeloId($idPaginaSelecionada);
+        $dolarFotoBanner = $this->paginaDinamicaModel->listarFotoBannerPeloId($idPaginaSelecionada);
+        $dolarFotoPergunta = $this->paginaDinamicaModel->listarFotoPerguntaPeloId($idPaginaSelecionada);
+        $dolarFotoServico = $this->paginaDinamicaModel->listarFotoServicoPeloId($idPaginaSelecionada);
+        $dolarFotoTexto = $this->paginaDinamicaModel->listarFotoTextoPeloId($idPaginaSelecionada);
+
+        $dados = [
+            'paginas' => $paginas,
+            'tituloBreadcrumb' => 'cabeamento_estruturado' ,
+            'paginaSelecionada' => $paginaSelecionada,
+            'dolarFotoBanner' => $dolarFotoBanner,
+            'dolarFotoPergunta' => $dolarFotoPergunta,
+            'dolarFotoServico' => $dolarFotoServico,
+            'dolarFotoTexto' => $dolarFotoTexto
+        ];
+
+        $this->view('paginas/cabeamento_estruturado', $dados);
+    }
+        
+    public function solucoes_de_nobreaks() { 
+
+        $idPaginaSelecionada = 88;
+        $paginas = $this->paginaDinamicaModel->listarPaginasAtivas();
+        $paginaSelecionada = $this->paginaDinamicaModel->listarPaginasPeloId($idPaginaSelecionada);
+        $dolarFotoBanner = $this->paginaDinamicaModel->listarFotoBannerPeloId($idPaginaSelecionada);
+        $dolarFotoPergunta = $this->paginaDinamicaModel->listarFotoPerguntaPeloId($idPaginaSelecionada);
+        $dolarFotoServico = $this->paginaDinamicaModel->listarFotoServicoPeloId($idPaginaSelecionada);
+        $dolarFotoTexto = $this->paginaDinamicaModel->listarFotoTextoPeloId($idPaginaSelecionada);
+
+        $dados = [
+            'paginas' => $paginas,
+            'tituloBreadcrumb' => 'solucoes_de_nobreaks' ,
+            'paginaSelecionada' => $paginaSelecionada,
+            'dolarFotoBanner' => $dolarFotoBanner,
+            'dolarFotoPergunta' => $dolarFotoPergunta,
+            'dolarFotoServico' => $dolarFotoServico,
+            'dolarFotoTexto' => $dolarFotoTexto
+        ];
+
+        $this->view('paginas/solucoes_de_nobreaks', $dados);
+    }
+        
+    public function solucoes_em_nuvem() { 
+
+        $idPaginaSelecionada = 89;
+        $paginas = $this->paginaDinamicaModel->listarPaginasAtivas();
+        $paginaSelecionada = $this->paginaDinamicaModel->listarPaginasPeloId($idPaginaSelecionada);
+        $dolarFotoBanner = $this->paginaDinamicaModel->listarFotoBannerPeloId($idPaginaSelecionada);
+        $dolarFotoPergunta = $this->paginaDinamicaModel->listarFotoPerguntaPeloId($idPaginaSelecionada);
+        $dolarFotoServico = $this->paginaDinamicaModel->listarFotoServicoPeloId($idPaginaSelecionada);
+        $dolarFotoTexto = $this->paginaDinamicaModel->listarFotoTextoPeloId($idPaginaSelecionada);
+
+        $dados = [
+            'paginas' => $paginas,
+            'tituloBreadcrumb' => 'solucoes_em_nuvem' ,
+            'paginaSelecionada' => $paginaSelecionada,
+            'dolarFotoBanner' => $dolarFotoBanner,
+            'dolarFotoPergunta' => $dolarFotoPergunta,
+            'dolarFotoServico' => $dolarFotoServico,
+            'dolarFotoTexto' => $dolarFotoTexto
+        ];
+
+        $this->view('paginas/solucoes_em_nuvem', $dados);
     }
 }
