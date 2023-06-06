08f642f5b40a211f0158c2794277eceea065857d
conuaz <lucas.olasz@tty2000.com.br>
2023-06-06 18:05:09 -0300

    Ajusta tamanho dos logos das parcerias

--- app/Views/lider_pro/parcerias.php
+++ app/Views/lider_pro/parcerias.php
@@ -2,35 +2,33 @@
 
     <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
 
-    <div class="row mt-5">
-        <div class="row">
-            <div class="col-md-4 col-lg-4  p-2">
-                <img class="img-fluid" src="<?= URL . '/img/menu_lider_pro/parceria1.png' ?>" alt="">
-            </div>
-            <div class="col-md-4 col-lg-4  p-2">
-                <img class="img-fluid" src="<?= URL . '/img/menu_lider_pro/parceria2.png' ?>" alt="">
-            </div>
-            <div class="col-md-4 col-lg-4  p-2">
-                <img class="img-fluid" src="<?= URL . '/img/menu_lider_pro/parceria3.png' ?>" alt="">
-            </div>
+    <div class="row d-flex align-items-center justify-content-center mt-5">
+        <div class="col-md-4 col-lg-4">
+            <img class="img-fluid lp-parcerias" src="<?= URL . '/img/menu_lider_pro/parceria1.png' ?>" alt="">
         </div>
-
-        <div class="row">
-            <div class="col-md-4 col-lg-4  p-5">
-                <img class="img-fluid" src="<?= URL . '/img/menu_lider_pro/parceria4.png' ?>" alt="">
-            </div>
-            <div class="col-md-4 col-lg-4  p-2">
-                <img class="img-fluid" src="<?= URL . '/img/menu_lider_pro/parceria5.png' ?>" alt="">
-            </div>
-            <div class="col-md-4 col-lg-4  p-2">
-                <img class="img-fluid" src="<?= URL . '/img/menu_lider_pro/parceria6.png' ?>" alt="">
-            </div>
+        <div class="col-md-4 col-lg-4">
+            <img class="img-fluid lp-parcerias" src="<?= URL . '/img/menu_lider_pro/parceria2.png' ?>" alt="">
         </div>
+        <div class="col-md-4 col-lg-4">
+            <img class="img-fluid lp-parcerias" src="<?= URL . '/img/menu_lider_pro/parceria3.png' ?>" alt="">
+        </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-4 col-lg-4  p-2">
-                <img class="img-fluid" src="<?= URL . '/img/menu_lider_pro/parceria7.png' ?>" alt="">
-            </div>
+    <div class="row d-flex align-items-center justify-content-center mt-5">
+        <div class="col-md-4 col-lg-4">
+            <img class="img-fluid lp-parcerias" src="<?= URL . '/img/menu_lider_pro/parceria4.png' ?>" alt="">
+        </div>
+        <div class="col-md-4 col-lg-4">
+            <img class="img-fluid lp-parcerias" src="<?= URL . '/img/menu_lider_pro/parceria5.png' ?>" alt="">
+        </div>
+        <div class="col-md-4 col-lg-4">
+            <img class="img-fluid lp-parcerias" src="<?= URL . '/img/menu_lider_pro/parceria6.png' ?>" alt="">
+        </div>
+    </div>
+
+    <div class="row d-flex align-items-center justify-content-center mt-5">
+        <div class="col-md-4 col-lg-4">
+            <img class="img-fluid lp-parcerias" src="<?= URL . '/img/menu_lider_pro/parceria7.png' ?>" alt="">
         </div>
     </div>
 </div>
