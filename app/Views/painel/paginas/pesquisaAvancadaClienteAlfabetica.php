bbf59dd1bafe573368f71433ecfd60777d4eab14
conuaz <lucas.olasz@tty2000.com.br>
2023-06-07 16:03:44 -0300

    Ajusta responsividade

--- app/Views/painel/paginas/pesquisaAvancadaClienteAlfabetica.php
+++ app/Views/painel/paginas/pesquisaAvancadaClienteAlfabetica.php
@@ -2,30 +2,35 @@
 
     <div class="row d-flex justify-content-start mt-5">
         <div class="col-md-6 d-flex flex-row">
-            <div class="">
-                <a class="btn d-flex justify-content-center align-items-center lp-btn-liderpro m-2 <?= $dados['tituloBreadcrumb'] == 'ordemAlfabetica' ? 'lp-btn-liderpro-active' : ''; ?>" href="<?= URL . '/Paginas/pesquisaAvancadaClienteAlfabetica' ?>" role="button">POR ORDEM ALFABÉTICA</a>
-            </div>
-            <div>
-                <a class="btn d-flex justify-content-center align-items-center lp-btn-liderpro m-2 <?= $dados['tituloBreadcrumb'] == 'ordemSegmento' ? 'lp-btn-liderpro-active' : ''; ?>" href="<?= URL . '/Paginas/pesquisaAvancadaClientePorSegmento' ?>" role="button">POR ORDEM DE SEGMENTO</a>
+            <div class="row">
+                <div class="col">
+                    <a class="btn d-flex justify-content-center align-items-center lp-btn-liderpro m-2 <?= $dados['tituloBreadcrumb'] == 'ordemAlfabetica' ? 'lp-btn-liderpro-active' : ''; ?>" href="<?= URL . '/Paginas/pesquisaAvancadaClienteAlfabetica' ?>" role="button">POR ORDEM ALFABÉTICA</a>
+                </div>
+                <div class="col">
+                    <a class="btn d-flex justify-content-center align-items-center lp-btn-liderpro m-2 <?= $dados['tituloBreadcrumb'] == 'ordemSegmento' ? 'lp-btn-liderpro-active' : ''; ?>" href="<?= URL . '/Paginas/pesquisaAvancadaClientePorSegmento' ?>" role="button">POR ORDEM DE SEGMENTO</a>
+                </div>
             </div>
         </div>
         <div class="col-md-6 d-flex justify-content-end align-items-center">
-            <form class="form-inline mt-2 mt-md-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Pesquisar Cliente" aria-label="Pesquisar Cliente">
-                <button class="btn lp-btn-liderpro my-2 my-sm-0" type="submit">Pesquisar</button>
-            </form>
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Pesquisar">
+                <div class="input-group-append">
+                    <span class="input-group-text"><i class="bi bi-search"></i></span>
+                </div>
+            </div>
         </div>
     </div>
 
 
-    <div class="row mt-5">
-        <div class="mb-3 col-md-12 d-flex justify-content-between">
-            <a class="lp-remove-decoration opcao-alfabetica" id="1" >1</a>
+    <div class="d-flex flex-column mt-5">
+        <div class="mb-3 d-flex justify-content-between">
+            <a class="lp-remove-decoration opcao-alfabetica" id="1">1</a>
             <?php foreach ($dados['letrasAlfabeto'] as $letrasAlfabeto) { ?>
                 <a class="lp-remove-decoration opcao-alfabetica" id="<?= $letrasAlfabeto ?>"><?= $letrasAlfabeto ?></a>
             <?php } ?>
         </div>
-        <div class="col-md-12 lp-container-logos-avancado" id="divResultadoAjaxAlfabetica">
+
+        <div class="lp-container-logos-avancado" id="divResultadoAjaxAlfabetica">
             <p style="color: gray;" class="p-3">Escolha uma opção de filtro</p>
         </div>
     </div>
--- app/Views/painel/paginas/pesquisaAvancadaClientePorSegmento.php
+++ app/Views/painel/paginas/pesquisaAvancadaClientePorSegmento.php
@@ -2,32 +2,39 @@
 
     <div class="row d-flex justify-content-start mt-5">
         <div class="col-md-6 d-flex flex-row">
-            <div class="">
-                <a class="btn d-flex justify-content-center align-items-center lp-btn-liderpro m-2 <?= $dados['tituloBreadcrumb'] == 'ordemAlfabetica' ? 'lp-btn-liderpro-active' : ''; ?>" href="<?= URL . '/Paginas/pesquisaAvancadaClienteAlfabetica' ?>" role="button">POR ORDEM ALFABÉTICA</a>
-            </div>
-            <div>
-                <a class="btn d-flex justify-content-center align-items-center lp-btn-liderpro m-2 <?= $dados['tituloBreadcrumb'] == 'ordemSegmento' ? 'lp-btn-liderpro-active' : ''; ?>" href="<?= URL . '/Paginas/pesquisaAvancadaClientePorSegmento' ?>" role="button">POR ORDEM DE SEGMENTO</a>
+            <div class="row">
+                <div class="col">
+                    <a class="btn d-flex justify-content-center align-items-center lp-btn-liderpro m-2 <?= $dados['tituloBreadcrumb'] == 'ordemAlfabetica' ? 'lp-btn-liderpro-active' : ''; ?>" href="<?= URL . '/Paginas/pesquisaAvancadaClienteAlfabetica' ?>" role="button">POR ORDEM ALFABÉTICA</a>
+                </div>
+                <div class="col">
+                    <a class="btn d-flex justify-content-center align-items-center lp-btn-liderpro m-2 <?= $dados['tituloBreadcrumb'] == 'ordemSegmento' ? 'lp-btn-liderpro-active' : ''; ?>" href="<?= URL . '/Paginas/pesquisaAvancadaClientePorSegmento' ?>" role="button">POR ORDEM DE SEGMENTO</a>
+                </div>
             </div>
         </div>
         <div class="col-md-6 d-flex justify-content-end align-items-center">
-            <form class="form-inline mt-2 mt-md-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Pesquisar Cliente" aria-label="Pesquisar Cliente">
-                <button class="btn lp-btn-liderpro my-2 my-sm-0" type="submit">Pesquisar</button>
-            </form>
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Pesquisar">
+                <div class="input-group-append">
+                    <span class="input-group-text"><i class="bi bi-search"></i></span>
+                </div>
+            </div>
         </div>
     </div>
 
 
-    <div class="row mt-5" id="resultadoFiltro">
+    <div class="row mt-5">
         <div class="col-md-3">
             <h3 class="lp-titulo-paragrafo">SEGMENTOS</h3>
             <ul class="list-unstyled">
                 <?php foreach ($dados['segmentos'] as $segmentos) { ?>
-                    <li class="lp-paragrafo"><a class="lp-remove-decoration opcao-segmento" id="<?= $segmentos->id_segmento ?>" href="#"><?= $segmentos->ds_segmento ?></a></li>
+                    <li class="lp-paragrafo"><a class="lp-remove-decoration opcao-segmento" id="<?= $segmentos->id_segmento ?>"><?= $segmentos->ds_segmento ?></a></li>
                 <?php } ?>
             </ul>
         </div>
-        <div class="col-md-9 lp-container-logos-avancado" id="divResultadoAjaxPorSegmento"><p style="color: gray;" class="p-3">Escolha um segmento</p></div>
+
+        <div class="col-md-8 lp-container-logos-avancado m-3" id="divResultadoAjaxPorSegmento">
+            <p style="color: gray;" class="p-3">Escolha um segmento</p>
+        </div>
     </div>
 
 </div>
