5152bb5f773487ca596d71b45ccd7009ef2878a0
conuaz <lucas.olasz@tty2000.com.br>
2023-06-19 18:03:24 -0300

    Configura acesso banco hostgator

--- app/config/configuracao.php
+++ app/config/configuracao.php
@@ -1,35 +1,26 @@
 <?php 
 
 //Parametros para o Banco
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'projeto_base');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'projeto_base');
+// define('DB_PORT', '3306');
+// define('APP', dirname(dirname(__FILE__)));
+// define('URL', '/liderpro_mvc');
+
+
+
+define('DB_HOST', 'nspro74.hostgator.com.br');
+define('DB_USER', 'liderp09_liderpro');
+define('DB_PASS', 'DRvcg,6(3[O9');
+define('DB_NAME', 'liderp09_liderpro_mvc_hmg');
 define('DB_PORT', '3306');
 
-//dirname() retorna o nome do diretorio em que o arquivo está
 define('APP', dirname(dirname(__FILE__)));
+define('URL', 'https://liderweb.com.br/liderpro_hmg2');
 
-define('URL', '/liderpro_mvc');
 
 define('APP_NOME', 'Lider Pro - Informática Profissional');
-
 define('APP_VERSAO', '1.0.0');
 
-
-
-// //Parametros para o Banco
-// define('DB_HOST', 'nspro74.hostgator.com.br');
-// define('DB_USER', 'liderp09_liderpro');
-// define('DB_PASS', 'DRvcg,6(3[O9');
-// define('DB_NAME', 'liderp09_liderpro_mvc');
-// define('DB_PORT', '3306');
-
-// //dirname() retorna o nome do diretorio em que o arquivo está
-// define('APP', dirname(dirname(__FILE__)));
-
-// define('URL', 'https://liderweb.com.br/liderpro_hmg');
-
-// define('APP_NOME', 'Lider Pro - Informática Profissional');
-
-// define('APP_VERSAO', '1.0.0');
