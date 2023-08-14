1f1621c0e4d55c155cf8ee20bd3803bef180122c
lucasolasz <olaszlucas072@gmail.com>
2023-08-14 00:49:45 -0300

    Implementa envio de anexos no email

--- app/Controllers/EnvioEmail.php
+++ app/Controllers/EnvioEmail.php
@@ -11,6 +11,7 @@ class EnvioEmail extends Controller
     {
         $paginas = $this->paginaDinamicaModel->listarPaginasAtivas();
         $area = "Contato - Lider Pro";
+        $pathArquivoZip = "";
 
         $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         if (isset($formulario)) {
@@ -23,11 +24,22 @@ class EnvioEmail extends Controller
             $mensagem = trim($formulario['txtMensagem']);
             isset($formulario['enviarCopia']) ? $flagEnviaCopia = true : $flagEnviaCopia = false;
 
-            if (Email::EnviarEmailInterno($nome, $emailRemetente, $telefone, $emailEscolhidoDestinatario, $assunto, $mensagem, $area, $flagEnviaCopia)) {
+            $dados['fileAnexoContato'] = isset($_FILES['fileAnexoContato']) ? $_FILES['fileAnexoContato'] : "";
+
+            if (!$dados['fileAnexoContato']['name'][0] == "") {
+                $idPasta = uniqid();
+                $this->armazenaFotoAnexoContato($dados['fileAnexoContato'], $idPasta);
+                $pathArquivoZip = Zip::ziparPastaRetornaPathZip($idPasta);
+            }
+
+
+            if (Email::EnviarEmailInterno($nome, $emailRemetente, $telefone, $emailEscolhidoDestinatario, $assunto, $mensagem, $area, $flagEnviaCopia, $pathArquivoZip)) {
                 if (Email::EnviarEmailCliente($nome, $emailRemetente, $area)) {
+                    ApagaPasta::apagar($pathArquivoZip);
                     Alertas::mensagem('email', 'E-mail Enviado com sucesso');
                     Redirecionamento::redirecionar('Paginas/contatos');
                 } else {
+                    ApagaPasta::apagar($pathArquivoZip);
                     Alertas::mensagem('segmento', 'Não foi possível enviar o e-mail', 'alert alert-danger');
                     Redirecionamento::redirecionar('Paginas/contatos');
                 }
@@ -39,6 +51,28 @@ class EnvioEmail extends Controller
                 'paginas' => $paginas
             ];
             $this->view('painel/paginas/contatos', $dados);
+        }
+    }
+
+
+    public function armazenaFotoAnexoContato($dados, $idPasta)
+    {
+
+        $pastaArquivo = "anexos/anexo_" . $idPasta;
+        $upload = new Upload();
+        $tamanhoArray = count($dados['name']);
+
+        for ($i = 0; $i < $tamanhoArray; $i++) {
+
+            $arrayImagem = [
+                'name' => $dados['name'][$i],
+                'type' => $dados['type'][$i],
+                'tmp_name' => $dados['tmp_name'][$i],
+                'error' => $dados['error'][$i],
+                'size' => $dados['size'][$i],
+            ];
+
+            $upload->imagem($arrayImagem, NULL, $pastaArquivo);
         }
     }
 }
