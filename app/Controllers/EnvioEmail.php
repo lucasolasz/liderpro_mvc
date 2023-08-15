bb5d7d3bf9b0ef72c6fe4e8e093c3703b5387efb
lucasolasz <olaszlucas072@gmail.com>
2023-08-14 22:59:41 -0300

    Envia email assistência técnica tablet celular

--- app/Controllers/EnvioEmail.php
+++ app/Controllers/EnvioEmail.php
@@ -103,6 +103,31 @@ class EnvioEmail extends Controller
     
     }
 
+    public function enviarEmailAssistenciaTabletCelular(){
+
+        $assunto = "Avaliação - Tablet Celular";
+        $nomeRota = "tablet_celular_opc";
+
+        
+        $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        if (isset($formulario)) {
+        
+            $nome = trim($formulario['txtNome']);
+            $emailRemetente = trim($formulario['txtEmail']);
+            $telefone = trim($formulario['txtTelefone']);
+            // $emailEscolhidoDestinatario = trim($formulario['cboDestinatario']);
+            $equipamento = trim($formulario['txtEquipamento']);
+            $marcaModelo = trim($formulario['txtMarcaModelo']);
+            $mensagem = trim($formulario['txtMensagemEmail']);
+            isset($formulario['enviarCopia']) ? $flagEnviaCopia = true : $flagEnviaCopia = false;
+
+            $dados['fileAnexoAssistencia'] = isset($_FILES['fileAnexoAssistencia']) ? $_FILES['fileAnexoAssistencia'] : "";
+
+            $this->enviarEmailAssistenciaGenerico($assunto, $nome, $emailRemetente, $telefone, $equipamento, $marcaModelo, $mensagem, $flagEnviaCopia, $dados['fileAnexoAssistencia'], $nomeRota);
+        }
+    
+    }
+
     public function enviarEmailAssistenciaGenerico($assunto, $nome, $emailRemetente, $telefone, $equipamento, $marcaModelo, $mensagem, $flagEnviaCopia, $anexo, $nomeRota){
         
         $area = "Assistência - Liderpro";
