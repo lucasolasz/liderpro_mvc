ffbd6bf3be7700421880b7e057ef7f51f919cdf9
lucasolasz <olaszlucas072@gmail.com>
2023-08-20 23:19:02 -0300

    Inclui máscara para o telefone nos campos de email

--- app/Views/painel/paginas/contatos.php
+++ app/Views/painel/paginas/contatos.php
@@ -164,12 +164,27 @@
 </div>
 
 <script>
-
     $(document).ready(function() {
         $('#fileAnexoContato').on('change', function() {
             const fileCount = $(this).prop('files').length;
             $('#fileCount').text(`${fileCount} arquivo(s) selecionado(s)`);
         });
+
+
+        $("#txtTelefone")
+            .mask("(99) 9999-9999?9")
+            .focusout(function(event) {
+                var target, phone, element;
+                target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+                phone = target.value.replace(/\D/g, '');
+                element = $(target);
+                element.unmask();
+                if (phone.length > 10) {
+                    element.mask("(99) 99999-999?9");
+                } else {
+                    element.mask("(99) 9999-9999?9");
+                }
+            });
     });
 
     //Critica campos antes de salvar
