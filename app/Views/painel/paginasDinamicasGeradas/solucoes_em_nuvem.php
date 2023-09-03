126ccfa637134b0df3cbc94d881355ecf3938aaa
lucasolasz <olaszlucas072@gmail.com>
2023-09-02 23:48:55 -0300

    Atualiza páginas geradas

--- app/Views/painel/paginasDinamicasGeradas/solucoes_em_nuvem.php
+++ app/Views/painel/paginasDinamicasGeradas/solucoes_em_nuvem.php
@@ -1,6 +1,12 @@
 <?php
 //Criado para limitar os caracteres do texto principal do serviço
-$texto = $dados['paginaSelecionada'][0]->ds_texto_principal;
+if ($_SESSION['linguagem_selecionada'] == "PT") {
+    $texto = $dados['paginaSelecionada'][0]->ds_texto_principal;
+} elseif ($_SESSION['linguagem_selecionada'] == "ES") {
+    $texto = $dados['paginaSelecionada'][0]->ds_texto_principal_es;
+} elseif ($_SESSION['linguagem_selecionada'] == "EN") {
+    $texto = $dados['paginaSelecionada'][0]->ds_texto_principal_en;
+}
 
 // Limitar o texto a 800 caracteres
 $limiteCaracteres = 800;
@@ -84,187 +90,15 @@ $quantidadeTotalCaracteres = strlen($texto);
         <!-- <div class="col-sm-1 col-md-1 col-lg-1 lp-tab-servicos">SERVIÇOS</div> -->
 
         <div class="col-sm-11 col-md-11 col-lg-11">
-
-            <h2 class="py-3">Serviços</h2>
-
-            <!-- Tab links -->
-            <div class="tab-lp">
-                <button class="tablinks" onclick="openCity(event, 'Fotos')" id="defaultOpen">FOTOS</button>
-                <button class="tablinks" onclick="openCity(event, '<?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab1) ?>')"><?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab1) ?></button>
-                <button class="tablinks" onclick="openCity(event, '<?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab2) ?>')"><?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab2) ?></button>
-                <button class="tablinks" onclick="openCity(event, '<?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab3) ?>')"><?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab3) ?></button>
-                <button class="tablinks" onclick="openCity(event, '<?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab4) ?>')"><?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab4) ?></button>
-            </div>
-            <!-- Tab content -->
-            <div id="Fotos" class="tabcontent-lp">
-                <div class="container">
-                    <div class="row">
-                        <?php if (empty($dados['dolarFotoServico'])) { ?>
-                            <div class="col-md-4">
-                                <p>Em breve</p>
-                            </div>
-                            <?php  } else {
-                            foreach ($dados['dolarFotoServico'] as $dolarFotoServico) { ?>
-                                <div class="col-md-4">
-                                    <div class="card mb-4">
-                                        <img class="img-fluid" src="<?= URL . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR .  $dolarFotoServico->nm_path_arquivo . DIRECTORY_SEPARATOR .  $dolarFotoServico->nm_arquivo ?>" alt="">
-                                        <div class="card-body">
-                                        </div>
-                                    </div>
-                                </div>
-                        <?php }
-                        } ?>
-                    </div>
-                </div>
-            </div>
-
-            <div id="<?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab1) ?>" class="tabcontent-lp">
-                <div class="row">
-                    <div class="col-md-6 col-lg-6">
-                        <ol class="lp-lista-tab">
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico1tab1 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico1tab1 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico2tab1 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico2tab1 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico3tab1 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico3tab1 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico4tab1 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico4tab1 ?></li>
-                            <?php } ?>
-                        </ol>
-                    </div>
-                    <div class="col-md-6 col-lg-6">
-                        <ol class="lp-lista-tab">
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico5tab1 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico5tab1 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico6tab1 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico6tab1 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico7tab1 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico7tab1 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico8tab1 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico8tab1 ?></li>
-                            <?php } ?>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-
-            <div id="<?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab2) ?>" class="tabcontent-lp">
-                <div class="row">
-                    <div class="col-md-6 col-lg-6">
-                        <ol class="lp-lista-tab">
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico1tab2 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico1tab2 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico2tab2 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico2tab2 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico3tab2 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico3tab2 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico4tab2 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico4tab2 ?></li>
-                            <?php } ?>
-                        </ol>
-                    </div>
-                    <div class="col-md-6 col-lg-6">
-                        <ol class="lp-lista-tab">
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico5tab2 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico5tab2 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico6tab2 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico6tab2 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico7tab2 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico7tab2 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico8tab2 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico8tab2 ?></li>
-                            <?php } ?>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-
-            <div id="<?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab3) ?>" class="tabcontent-lp">
-                <div class="row">
-                    <div class="col-md-6 col-lg-6">
-                        <ol class="lp-lista-tab">
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico1tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico1tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico2tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico2tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico3tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico3tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico4tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico4tab3 ?></li>
-                            <?php } ?>
-                        </ol>
-                    </div>
-                    <div class="col-md-6 col-lg-6">
-                        <ol class="lp-lista-tab">
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico5tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico5tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico6tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico6tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico7tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico7tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico8tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico8tab3 ?></li>
-                            <?php } ?>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-
-            <div id="<?= mb_strtoupper($dados['paginaSelecionada'][0]->ds_tab4) ?>" class="tabcontent-lp">
-                <div class="row">
-                    <div class="col-md-6 col-lg-6">
-                        <ol class="lp-lista-tab">
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico1tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico1tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico2tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico2tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico3tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico3tab3 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico4tab3 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico4tab3 ?></li>
-                            <?php } ?>
-                        </ol>
-                    </div>
-                    <div class="col-md-6 col-lg-6">
-                        <ol class="lp-lista-tab">
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico5tab4 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico5tab4 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico6tab4 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico6tab4 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico7tab4 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico7tab4 ?></li>
-                            <?php } ?>
-                            <?php if ($dados['paginaSelecionada'][0]->ds_topico8tab4 != "") { ?>
-                                <li><?= $dados['paginaSelecionada'][0]->ds_topico8tab4 ?></li>
-                            <?php } ?>
-                        </ol>
-                    </div>
-                </div>
-            </div>
+            <?php
+            if ($_SESSION['linguagem_selecionada'] == "PT") {
+                include_once APP . '/Views/painel/paginas/tabs_pagina_pt.php';
+            } elseif ($_SESSION['linguagem_selecionada'] == "ES") {
+                include_once APP . '/Views/painel/paginas/tabs_pagina_es.php';
+            } elseif ($_SESSION['linguagem_selecionada'] == "EN") {
+                include_once APP . '/Views/painel/paginas/tabs_pagina_en.php';
+            }
+            ?>
         </div>
     </div>
 </div>
