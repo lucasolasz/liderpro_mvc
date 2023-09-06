612d4b84824770b11d02b898ca210ebb01f80494
lucasolasz <olaszlucas072@gmail.com>
2023-09-05 23:09:19 -0300

    Inclui páginas traduzidas de assistência

---
+++ app/Views/painel/paginas/assistencia_tecnica_es.php
@@ -0,0 +1,287 @@
+<div class="container p-5">
+    <div class="row">
+        <div class="col-md-12 col-lg-12 d-flex justify-content-center">
+            <h2 class="lp-titulo-paragrafo">Assistência Técnica Autorizada e Especializada em Equipamentos de Informática</h2>
+        </div>
+    </div>
+    <div class="row py-5">
+        ESPANHA
+        <div class="col-md-12 col-lg-12 d-flex justify-content-center">
+            <p class="lp-paragrafo ">Construímos um laboratório antiestático, com modernos equipamentos eletrônicos de precisão, bancadas de reparo e ferramentas de alta qualidade para executarmos da
+                forma tecnicamente correta e especializada os consertos eletrônicos em vários tipos de equipamentos. Com isso, ganhamos a confiança de fabricantes que nos concederam a
+                responsabilidade de sermos Assistência Técnica Autorizada dos seus equipamentos. Provendo, em seus centros de aprendizados, cursos especializados aos nossos técnicos.
+                <br />
+                <br />
+                Prestamos serviços de Assistência Técnica Preditiva, Preventiva, Corretiva e Instalação com atendimentos avulsos ou contratuais para:
+            </p>
+        </div>
+    </div>
+
+
+    <div class="d-flex flex-wrap justify-content-center align-items-end">
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/computador_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_computador.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-computador mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">Computador e Serviço</p>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_computador_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>Manutenção Preventiva</li>
+                            <li>Manutenção corretiva</li>
+                            <li>Recuperação de Dados</li>
+                            <li>Instalação de Programas</li>
+                            <li>Backup - Cópia Segura de Dados</li>
+                            <li>Upgrade de equipamento</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/notebook_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_notebook.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-notebook mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">Notebook</p>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_notebook_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>Manutenção Preventiva</li>
+                            <li>Manutenção corretiva</li>
+                            <li>Recuperação de Dados</li>
+                            <li>Instalação de Programas</li>
+                            <li>Backup -Cópia Segura de Dados</li>
+                            <li>Reparo de Peças Quebradas</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/tablet_celular_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_tablet_celular.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-tablet-celular mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">Tablet e Celular</p>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_tablet_celular_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>Manutenção Preventiva</li>
+                            <li>Manutenção corretiva</li>
+                            <li>Recuperação de Dados</li>
+                            <li>Instalação de Programas</li>
+                            <li>Backup -Cópia Segura de Dados</li>
+                            <li>Reparo de Peças Quebradas</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/projetor_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_projetor.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-projetor mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">Projetor</p>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_projetor_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>Manutenção Preventiva</li>
+                            <li>Manutenção corretiva</li>
+                            <li>Substituição de Lâmpada</li>
+                            <li>Reparo de Peças e Placas</li>
+                            <li>Instalação e Desinstalação no Local</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/fonte_alimentacao_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_fonte.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-fonte mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">Fonte de Alimentação</p>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_fonte_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>Manutenção Preventiva</li>
+                            <li>Reparo em Geral</li>
+                            <li>Tipo: AT, ATX, BTX, ITX e EPS</li>
+                            <li>Padrão PC, Servidor e Hotswap</li>
+                            <li>Backup -Cópia Segura de Dados</li>
+                            <li>Teste Digital de Voltagem, Potência e Tensão</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/tv_monitor_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_tv_monitor.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-tv-monitor mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">TV / Monitor</p>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_tv_monitor_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>Manutenção Corretiva</li>
+                            <li>Reparo de Peça, Placa e Fonte</li>
+                            <li>Substituição de Telas Quebradas</li>
+                            <li>Calibração de Imagem</li>
+                            <li>Instalação de Videowall</li>
+                            <li>Modelos de LED, LCD e Plasma</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/impressora_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_impressora.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-impressora mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">Impressora</p>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_impressora_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>Manutenção Preventiva</li>
+                            <li>Manutenção Corretiva</li>
+                            <li>Reparo de Peça, Placa e Fonte</li>
+                            <li>Instalação de Equipamentos</li>
+                            <li>Locação de Impressoras</li>
+                            <li>Manutenção Contratual</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/nobreak_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_nobreak.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-nobreak mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">No-Break</p>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_nobreak_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>Substituição de Baterias</li>
+                            <li>Calibração de Baterias</li>
+                            <li>Manutenção Preventiva</li>
+                            <li>Manutenção Corretiva</li>
+                            <li>Manutenção Contratual</li>
+                            <li>Avaliação Técnica no Local</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-3">
+            <a href="<?= URL . '/AssistenciaOpcao/apple_opc' ?>">
+                <div class="d-flex justify-content-center align-items-end">
+                    <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_apple.png' ?>" alt="">
+                </div>
+                <div class="lp-background-assistencia-apple mt-2 div-card-assistencia">
+                    <div class="d-flex justify-content-center">
+                        <p class="my-2">Mac - IPad - IPhone</p>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <img class="lp-imagem-texto-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_apple_mini.png' ?>" alt="">
+                    </div>
+
+                    <div class="mt-2">
+                        <ul class="px-4">
+                            <li>MacBook, iMac, Mac Pro e Mac Mini</li>
+                            <li>Manutenção Preventiva e Corretiva</li>
+                            <li>Recuperação de Dados</li>
+                            <li>Atualização de Software</li>
+                            <li>Manutenção Contratual</li>
+                            <li>Substituição de peças</li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- 
+        
+        
+
+    
+
+
+
+        
+
+        
+
+        <div class="d-flex justify-content-center align-items-end">
+            <a href="<?= URL . '/AssistenciaOpcao/apple_opc' ?>">
+                <img class="img-fluid img-fluid-assistencia" src="<?= URL . '/img/paginas/assistencia_tecnica/assistencia_tecnica_apple.png' ?>" alt="">
+            </a>
+        </div> -->
+
+    </div>
+</div>
