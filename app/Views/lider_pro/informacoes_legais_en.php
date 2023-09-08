eceb2a86187a329da9e4f03cbeb8b59c63ee1308
lucasolasz <olaszlucas072@gmail.com>
2023-09-08 20:09:13 -0300

    Traduz informacoes legais

---
+++ app/Views/lider_pro/informacoes_legais_en.php
@@ -0,0 +1,110 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <div class="row p-5">
+        <div class="col-md-12 col-lg-12 d-flex  justify-content-center">
+            <p class="lp-paragrafo-sublinhado">LEGAL INFORMATION AND COMMUNICATIONS</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 col-lg-6">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-1 col-lg-1">
+                    <span><img style="height: 30px; width: 24px;" class="" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_foto1.png' ?>" alt=""></span>
+                </div>
+                <div class="col-md-7 col-lg-7 lp-paragrafo">
+
+                    <p class="lp-paragrafo-info">
+                        CORPORATE NAME: Liderpro Informática Ltda.
+                        <br>
+
+                        FANTASY NAME: Liderpro Informática
+                        <br>
+                        CNPJ: 03.455.202/0001-16
+                        <br>
+
+                        STATE REGISTRATION: 76.078.599
+                        <br>
+
+                        MUNICIPAL REGISTRATION: 02.615.851
+                        <br>
+
+                        OPENING DATE: 11/out/1999.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row mt-5 d-flex justify-content-center mb-5 pt-4">
+                <div class="col-md-1 col-lg-1">
+                    <span><img style="height: 40px; width: 24px;" class="" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_foto2.png' ?>" alt=""></span>
+                </div>
+                <div class="col-md-7 col-lg-7 ">
+                    <p class="lp-paragrafo-info">
+                        ADDRESS: Rua Conde Lages, 44 / sala 402
+                        <br>
+                        NEIGHBORHOOD: Glória
+                        <br>
+                        CITY: Rio de Janeiro
+                        <br>
+                        STATE: Rio de Janeiro (RJ)
+                        <br>
+                        CEP: 20.241-080
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-6">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-1 col-lg-1">
+                    <span><img style="height: 25px; width: 24px;" class="" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_foto3.png' ?>" alt=""></span>
+                </div>
+                <div class="col-md-7 col-lg-7 ">
+
+                    <p class="lp-paragrafo-info">
+
+                        WEBSITES and SOCIAL NETWORKS:
+
+                        <a href="#">www.liderpro.com.br</a>
+                        <br>
+                        <a href="#">www.liderpro.com</a>
+                        <br>
+                        <a href="#">www.liderweb.com.br</a>
+
+                    <div class="row py-2">
+                        <div class="p-1"><a href="<?= URL_FACEBOOK ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_face.png' ?>" alt=""></a> </div>
+                        <div class="p-1"><a href="<?= URL_INSTAGRAM ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_insta.png' ?>" alt=""></a> </div>
+                        <div class="p-1"><a href="<?= URL_YOUTUBE ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_youtube.png' ?>" alt=""></a> </div>
+                        <div class="p-1"><a href="<?= URL_LINKEDIN ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_linkedin.png' ?>" alt=""></a> </div>
+                        <div class="p-1"><a href="<?= URL_TWITTER ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_twitter.png' ?>" alt=""></a> </div>
+                    </div>
+                    </p>
+                </div>
+            </div>
+            <div class="row mt-5 d-flex justify-content-center">
+                <div class="col-md-8 col-lg-8 d-flex flex-column">
+                    <div>
+                        <p class="lp-paragrafo-info m-0"><span><img style="height: 20px; width: 20px;" class="mr-2 p-1" src="<?= URL . '/img/menu_lider_pro/telefone.png' ?>" alt=""></span><a href="https://wa.me/552125268100?text=Ol%C3%A1%21" target="_blank">(21) 2526-8100</a> <span><img style="height: 20px; width: 20px;" class="ml-2 p-1" src="<?= URL . '/img/menu_lider_pro/whatsapp.png' ?>" alt=""></span></p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0"><span><img style="height: 20px; width: 20px;" class="mr-2 p-1" src="<?= URL . '/img/menu_lider_pro/email.png' ?>" alt=""></span><a href="mailto:liderpro@liderpro.com.br">liderpro@liderpro.com.br</a></p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0"><span><img style="height: 20px; width: 20px;" class="mr-2 p-1" src="<?= URL . '/img/menu_lider_pro/relogio.png' ?>" alt=""></span>HOURS OF OPERATION:</p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0">From 8:30 am to 6:00 pm - From Monday to Friday, except holidays.</p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0">Obs.: On Thursdays there is a street market until 4pm.</p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0">Access to the garage via Rua Visc. from Paranaguá.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ app/Views/lider_pro/informacoes_legais_es.php
@@ -0,0 +1,110 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <div class="row p-5">
+        <div class="col-md-12 col-lg-12 d-flex  justify-content-center">
+            <p class="lp-paragrafo-sublinhado">INFORMACIÓN LEGAL Y COMUNICACIONES</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 col-lg-6">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-1 col-lg-1">
+                    <span><img style="height: 30px; width: 24px;" class="" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_foto1.png' ?>" alt=""></span>
+                </div>
+                <div class="col-md-7 col-lg-7 lp-paragrafo">
+
+                    <p class="lp-paragrafo-info">
+                    RAZÓN SOCIAL: Liderpro Informática Ltda.
+                        <br>
+
+                        NOMBRE DE FANTASÍA: Liderpro Informática
+                        <br>
+                        CNPJ: 03.455.202/0001-16
+                        <br>
+
+                        INSCRIPCIÓN ESTADUAL: 76.078.599
+                        <br>
+
+                        REGISTRO MUNICIPAL: 02.615.851
+                        <br>
+
+                        FECHA DE APERTURA: 11/out/1999.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row mt-5 d-flex justify-content-center mb-5 pt-4">
+                <div class="col-md-1 col-lg-1">
+                    <span><img style="height: 40px; width: 24px;" class="" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_foto2.png' ?>" alt=""></span>
+                </div>
+                <div class="col-md-7 col-lg-7 ">
+                    <p class="lp-paragrafo-info">
+                    DIRECCIÓN: Rua Conde Lages, 44 / sala 402
+                        <br>
+                        VECINDARIO: Glória
+                        <br>
+                        CIUDAD: Rio de Janeiro
+                        <br>
+                        ESTADO: Rio de Janeiro (RJ)
+                        <br>
+                        Código postal: 20.241-080
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-6">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-1 col-lg-1">
+                    <span><img style="height: 25px; width: 24px;" class="" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_foto3.png' ?>" alt=""></span>
+                </div>
+                <div class="col-md-7 col-lg-7 ">
+
+                    <p class="lp-paragrafo-info">
+
+                    SITIOS WEB y REDES SOCIALES:
+
+                        <a href="#">www.liderpro.com.br</a>
+                        <br>
+                        <a href="#">www.liderpro.com</a>
+                        <br>
+                        <a href="#">www.liderweb.com.br</a>
+
+                    <div class="row py-2">
+                        <div class="p-1"><a href="<?= URL_FACEBOOK ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_face.png' ?>" alt=""></a> </div>
+                        <div class="p-1"><a href="<?= URL_INSTAGRAM ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_insta.png' ?>" alt=""></a> </div>
+                        <div class="p-1"><a href="<?= URL_YOUTUBE ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_youtube.png' ?>" alt=""></a> </div>
+                        <div class="p-1"><a href="<?= URL_LINKEDIN ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_linkedin.png' ?>" alt=""></a> </div>
+                        <div class="p-1"><a href="<?= URL_TWITTER ?>" target="_blank"><img style="height: 30px; width: 30px;" src="<?= URL . '/img/menu_lider_pro/informacoes_legais_rede_twitter.png' ?>" alt=""></a> </div>
+                    </div>
+                    </p>
+                </div>
+            </div>
+            <div class="row mt-5 d-flex justify-content-center">
+                <div class="col-md-8 col-lg-8 d-flex flex-column">
+                    <div>
+                        <p class="lp-paragrafo-info m-0"><span><img style="height: 20px; width: 20px;" class="mr-2 p-1" src="<?= URL . '/img/menu_lider_pro/telefone.png' ?>" alt=""></span><a href="https://wa.me/552125268100?text=Ol%C3%A1%21" target="_blank">(21) 2526-8100</a> <span><img style="height: 20px; width: 20px;" class="ml-2 p-1" src="<?= URL . '/img/menu_lider_pro/whatsapp.png' ?>" alt=""></span></p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0"><span><img style="height: 20px; width: 20px;" class="mr-2 p-1" src="<?= URL . '/img/menu_lider_pro/email.png' ?>" alt=""></span><a href="mailto:liderpro@liderpro.com.br">liderpro@liderpro.com.br</a></p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0"><span><img style="height: 20px; width: 20px;" class="mr-2 p-1" src="<?= URL . '/img/menu_lider_pro/relogio.png' ?>" alt=""></span>HORAS DE OFICINA:</p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0">8:30 am a 6:00 pm - Lunes a viernes, excepto festivos.</p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0">Obs.: Los jueves hay mercadillo hasta las 16 horas.</p>
+                    </div>
+                    <div>
+                        <p class="lp-paragrafo-info m-0">Acceso al garaje por Rua Visc. de Paranaguá.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
