9078a8a9df9026a9d9640237faf6cf221f54e729
lucasolasz <olaszlucas072@gmail.com>
2023-09-08 19:48:06 -0300

    Traduz valores

---
+++ app/Views/lider_pro/valores_en.php
@@ -0,0 +1,45 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <p class="lp-paragrafo mt-5">
+        <span class="lp-titulo-paragrafo"> Our mission </span>
+        <br>
+        Offer IT services with technological innovation and excellent service, always seeking the total professional satisfaction of our customers, suppliers and employees.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Our vision </span>
+        <br>
+        Contribute to a more humane, fair and environmentally responsible world. Seeking the improvement of society through respect for others and the evolution of science and technology.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Honesty and Trust </span>
+        <br>
+        They are more than mere words, they are attitudes and behaviors that govern relations with our customers, employees and suppliers. We honor the commitments assumed, demonstrating our integrity and transparency in all negotiations, projects and service provision.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Respect for the law </span>
+        <br>
+        Comply with all legislation, standards and regulatory obligations, both fiscal and technical standards of the national institute (ABNT) and international (IEEE, EIA and TIA). This is the way in which our company competes fully in the market, respecting the rights and duties of consumers and our legislation.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Integrity </span>
+        <br>
+        Our ethical conduct -corporate and individual- guides attitudes such as:
+        <br>
+        1. Give up personal benefits or advantages for oneself or the company, through gifts, invitations or treats that influence commercial decisions;
+        <br>
+        2. Use for your own benefit or pass on to third parties, documents, tools, methods, products, services and information owned by customers.
+        Although we recognize the importance of political activity for decision-making regarding public policies in the country, our company chooses not to make donations of any kind to political parties or any organizations, public or private, that act directly or indirectly in political-political activities. partisans.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Human rights </span>
+        <br>
+        Promote opportunities so that men and women, regardless of their age, marital status, sexual orientation, race, color, nationality, religion, gender, disability and ethnicity, can have access to decent and productive work, in conditions of freedom, equity, safety and dignity - in accordance with the main mission of the International Labor Organization (ILO) - and respecting the principles of the United Nations Universal Declaration of Human Rights, are guidelines adopted by our company.
+    </p>
+</div>
---
+++ app/Views/lider_pro/valores_es.php
@@ -0,0 +1,45 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <p class="lp-paragrafo mt-5">
+        <span class="lp-titulo-paragrafo"> Nuestra misión </span>
+        <br>
+        Ofrecer servicios TI con innovación tecnológica y excelente servicio, buscando siempre la total satisfacción profesional de nuestros clientes, proveedores y empleados.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Nuestra visión </span>
+        <br>
+        Contribuir a un mundo más humano, justo y ambientalmente responsable. Buscar la mejora de la sociedad a través del respeto a los demás y la evolución de la ciencia y la tecnología.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Honestidad y confianza </span>
+        <br>
+        Son más que simples palabras, son actitudes y comportamientos que rigen las relaciones con nuestros clientes, empleados y proveedores. Honramos los compromisos asumidos, demostrando nuestra integridad y transparencia en todas las negociaciones, proyectos y prestación de servicios.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Respeto a la ley </span>
+        <br>
+        Cumplir con toda la legislación, normas y obligaciones regulatorias, tanto fiscales como técnicas del instituto nacional (ABNT) e internacionales (IEEE, EIA y TIA). Así es como nuestra empresa compite plenamente en el mercado, respetando los derechos y deberes de los consumidores y nuestra legislación.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Integridad </span>
+        <br>
+        Nuestra conducta ética -corporativa e individual- orienta actitudes tales como:
+        <br>
+        1. Renunciar a beneficios o ventajas personales para sí o para la empresa, mediante obsequios, invitaciones o golosinas que influyan en las decisiones comerciales;
+        <br>
+        2. Utilizar en beneficio propio o transmitir a terceros documentos, herramientas, métodos, productos, servicios e información propiedad de los clientes.
+        Si bien reconocemos la importancia de la actividad política para la toma de decisiones respecto de las políticas públicas del país, nuestra empresa opta por no realizar donaciones de ningún tipo a partidos políticos ni a ninguna organización, pública o privada, que actúe directa o indirectamente en actividades políticas partidistas.
+    </p>
+
+    <p class="lp-paragrafo">
+        <span class="lp-titulo-paragrafo"> Derechos humanos </span>
+        <br>
+        Promover oportunidades para que hombres y mujeres, independientemente de su edad, estado civil, orientación sexual, raza, color, nacionalidad, religión, género, discapacidad y etnia, tengan acceso a un trabajo digno y productivo, en condiciones de libertad, equidad, seguridad y dignidad - de acuerdo con la misión principal de la Organización Internacional del Trabajo (OIT) - y respetando los principios de la Declaración Universal de Derechos Humanos de las Naciones Unidas, son directrices adoptadas por nuestra empresa.
+    </p>
+</div>
