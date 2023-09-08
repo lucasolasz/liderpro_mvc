054cc83a3d0992fa4dde1f7263769e06ef56409d
lucasolasz <olaszlucas072@gmail.com>
2023-09-08 19:57:28 -0300

    Traduz politica de privacidade

---
+++ app/Views/lider_pro/politica_de_privacidade_en.php
@@ -0,0 +1,100 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <div class="row p-5">
+        <div class="col-md-12 col-lg-12 d-flex  justify-content-center">
+            <p class="lp-paragrafo-sublinhado">PRIVACY POLICY</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-lg-12">
+            <p class="lp-paragrafo">
+                LIDERPRO Informática's main criterion is the individuality and privacy of its website users. That's why we've established a strict policy to protect your personal information as securely as possible.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo">How do we collect your personal and/or business information?</span>
+                <br>
+                Personal information is requested by Liderpro Informática when:
+                <br>
+                &#160;1. User Registration, with data for receiving Electronic News or other information (if you wish to completely cancel receiving emails, the user will have this option at the end of each email);
+                <br>
+                &#160;2. Individual Customer Registration, with data for filling out the Invoice and information (username and password) to access the online database;
+                <br>
+                &#160;3. Service Contracting Customer Registration, with data for filling out the Invoice, Legal Contract and information (username and password) to access the online database;
+                <br>
+                &#160;4. Registration of Suppliers, with data for completing the Invoice, payments or bank deposits and/or Legal Contract;
+                <br>
+                &#160;5. Employee Registration, with data for filling out the Employment Contract and personal information requested by the Personnel Department and/or Human Resources Department;
+                <br>
+                &#160;6. CV Registration, with data to fill in the personal and professional information of candidates who intend to participate in our CV database;
+                <br>
+                &#160;7. Cookie files, when visiting our website and which can be easily changed in the configuration of each browser by the user.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> What do we do with your personal information?</span>
+                <br>
+                The information provided by you will be kept strictly confidential and will only be used to streamline your customer relationship and help us to improve our website for you. We do not provide, publish or market your personal and/or professional information to other companies or individuals without your permission, except:
+                <br>
+                &#160;1. If you place an order –product and/or service- that we need to send by freight and, therefore, we will have to provide your data to the shipping company;
+                <br>
+                &#160;2. Or very special cases, such as court orders for the provision of a particular user's data;
+                <br>
+                &#160;3. Or in situations where it is necessary to disclose certain data, necessary to identify, contact or take legal action against someone who (intentionally or not) is using our services for illegal purposes.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo">Update Registration</span>
+                <br>
+                You can help Liderpro Informática to maintain the accuracy of your information, notifying us of changes in your personal data. You only need to access one of the Electronic Registration Forms (Individual Customer, Corporate Customer or Supplier), in the REGISTRATION section, and change only the data that must be updated. Then press the Save key.
+                <br>
+                If you have any questions or would like us to change your data for you, send an email to liderpro@liderpro.com.br or call (21) 2526-8100.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Cookies</span>
+                <br>
+                Every time the user accesses the Liderpro Informática website and its subsequent sections (sites), it will systematically record the time, user identification, each operation performed and the IP number. With this, we identify the areas that are of greatest interest to users, thus being able to direct more attention and investment.
+                <br>
+                Note: IP is an identification number of your computer on the Internet
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo">SPAM</span>
+                <br>
+                Possibly at some point in your life on the Internet, you must have already received, in your mailbox (e-mail), advertising messages coming from people or companies that you have never heard of. This deplorable practice is known as "spam", and unfortunately it is common on the Internet. Liderpro Informática does not use these devices. Users do not receive unsolicited advertising emails at any time. All emails sent to our users are sent for one of the following reasons:
+                <br>
+                &#160;1. To notify you of information relating to the website and its operation;
+                <br>
+                &#160;2. To send information requested by the user;
+                <br>
+                &#160;3. To communicate changes to the service or pertinent information to the user;
+                <br>
+                &#160;4. To communicate the availability of one or more job vacancies in our company.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> What tools do we use to keep your personal information secure?</span>
+                <br>
+                Every time the user fills out any of the electronic registration or resume forms available on our website and sends it to us, the data will be automatically encrypted. As a result, from the moment the data leaves your computer until it reaches our server, it will be unrecognizable. After reaching our server, the data will be decrypted and stored legibly.
+                <br>
+                For this process to occur in the most perfect security, we use TLS encryption technology. In addition to using Locaweb as a website hosting provider; one of the best, largest and most secure website hosting in Brazil.
+                <br>
+                Note: TLS (Transport Layer Secure) are encrypted forms (protocols) that provide communication security when using the Internet.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo">General information</span>
+                <br>
+                The websites that we indicate on our pages, we do not have any kind of control over them. We advise you to try to check the privacy policy of each of these websites. Due to the rapid evolution of technologies on the Internet, we may occasionally update this policy or part of it, and should therefore be checked periodically.
+            </p>
+
+            <p class="lp-paragrafo">
+                Liderpro Informática needs your help with comments, suggestions and questions about this policy or any other area of ​​our website.
+            </p>
+        </div>
+    </div>
+</div>
---
+++ app/Views/lider_pro/politica_de_privacidade_es.php
@@ -0,0 +1,100 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <div class="row p-5">
+        <div class="col-md-12 col-lg-12 d-flex  justify-content-center">
+            <p class="lp-paragrafo-sublinhado">POLÍTICA DE PRIVACIDAD</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-lg-12">
+            <p class="lp-paragrafo">
+                El principal criterio de LIDERPRO Informática es la individualidad y privacidad de los usuarios de su sitio web. Es por eso que hemos establecido una política estricta para proteger su información personal de la forma más segura posible.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo">¿Cómo recopilamos su información personal y/o comercial?</span>
+                <br>
+                Los datos personales son solicitados por Liderpro Informática cuando:
+                <br>
+                &#160;1. Registro de Usuario, con datos para recibir Noticias Electrónicas u otra información (si desea cancelar por completo la recepción de correos electrónicos, el usuario tendrá esta opción al final de cada correo electrónico);
+                <br>
+                & # 160; 2. Registro de Cliente Individual, con datos para completar la Factura e información (usuario y contraseña) para acceder a la base de datos en línea;
+                <br>
+                & # 160; 3. Registro de Cliente de Contratación de Servicios, con datos para completar la Factura, Contrato Legal e información (usuario y contraseña) para acceder a la base de datos en línea;
+                <br>
+                & # 160; 4. Registro de Proveedores, con datos para diligenciamiento de Facturas, pagos o depósitos bancarios y/o Contrato Legal;
+                <br>
+                &#160;5. Registro de Empleados, con datos para completar el Contrato de Trabajo e información personal solicitada por el Departamento de Personal y/o el Departamento de Recursos Humanos;
+                <br>
+                &#160;6. Registro de Currículum, con datos para completar información personal y profesional de los candidatos que pretenden participar en nuestra base de datos de currículums;
+                <br>
+                &#160;7. Archivos de cookies, a través de la visita a nuestro sitio web y que pueden ser fácilmente modificados en la configuración de cada navegador por parte del usuario.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> ¿Qué hacemos con tu información personal?</span>
+                <br>
+                La información proporcionada por usted se mantendrá estrictamente confidencial y solo se utilizará para optimizar su relación con el cliente y ayudarnos a mejorar nuestro sitio web para usted. No proporcionamos, publicamos ni comercializamos su información personal y/o profesional a otras empresas o personas sin su permiso, excepto:
+                <br>
+                &#160;1. Si realiza un pedido –producto y/o servicio- que necesitamos enviar por flete y, por tanto, tendremos que facilitar sus datos a la empresa de transporte;
+                <br>
+                & # 160; 2. O casos muy especiales, como determinaciones judiciales por el suministro de datos de un determinado usuario;
+                <br>
+                & # 160; 3. O en situaciones en las que sea necesario revelar ciertos datos, necesarios para identificar, contactar o emprender acciones legales contra alguien que (intencionalmente o no) está utilizando nuestros servicios con fines ilegales.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo">Actualizar registro</span>
+                <br>
+                Puede ayudar a Liderpro Informática a mantener la exactitud de su información notificándonos los cambios en sus datos personales. Sólo necesita acceder a uno de los Formularios de Registro Electrónico (Cliente Individual, Cliente Corporativo o Proveedor), en la sección REGISTRO, y cambiar únicamente los datos que deban estar actualizados. Luego presione la tecla Guardar.
+                <br>
+                Si tiene alguna pregunta o desea que modifiquemos sus datos por usted, envíe un correo electrónico a liderpro@liderpro.com.br o llame al (21) 2526-8100.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Cookies</span>
+                <br>
+                Cada vez que el usuario acceda al sitio web de Liderpro Informática y sus posteriores secciones (sitios), estará registrando sistemáticamente la hora, la identificación del usuario, cada operación realizada y el número de IP. Con ello identificamos las áreas que son de mayor interés para los usuarios, pudiendo así dirigir más atención e inversión.
+                <br>
+                Nota: IP es un número de identificación de su computadora en Internet.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo">SPAM</span>
+                <br>
+                Posiblemente en algún momento de tu vida en Internet ya hayas recibido, en la bandeja de entrada de tu correo electrónico, mensajes publicitarios de personas o empresas de las que nunca habías oído hablar. Esta deplorable práctica se conoce como “spam”, y lamentablemente es común en Internet. Liderpro Informática no utiliza estos dispositivos. Los usuarios no reciben en ningún momento correos electrónicos publicitarios no solicitados. Todos los correos electrónicos enviados a nuestros usuarios se envían por uno de los siguientes motivos:
+                <br>
+                &#160;1. Para notificarle información relacionada con el sitio web y su funcionamiento;
+                <br>
+                & # 160; 2. Para enviar información solicitada por el usuario;
+                <br>
+                & # 160; 3. Comunicar cambios en el servicio o información pertinente al usuario;
+                <br>
+                & # 160; 4. Comunicar la disponibilidad de una o más ofertas de empleo en nuestra empresa.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> ¿Qué herramientas utilizamos para mantener segura su información personal?</span>
+                <br>
+                Cada vez que el usuario rellene cualquiera de los formularios electrónicos de inscripción o currículum disponibles en nuestra web y nos lo envíe, los datos quedarán cifrados automáticamente. Por lo tanto, desde el momento en que los datos salen de su ordenador hasta que llegan a nuestro servidor, serán irreconocibles. Después de llegar a nuestro servidor, los datos serán descifrados y almacenados de forma legible.
+                <br>
+                Para que este proceso se produzca con la mayor seguridad, utilizamos tecnología de cifrado TLS. Además de utilizar la empresa Locaweb como proveedor de hosting de sitios web; uno de los mejores, más grandes y más seguros servidores de sitios web de Brasil.
+                <br>
+                Nota: TLS (Transport Layer Secure) son formularios (protocolos) cifrados que brindan seguridad en las comunicaciones al utilizar Internet.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo">Informaciones generales</span>
+                <br>
+                Los sitios web que indicamos en nuestras páginas, no tenemos ningún tipo de control sobre los mismos. Le aconsejamos que intente consultar la política de privacidad de cada uno de estos sitios web. Debido a la rápida evolución de las tecnologías en Internet, ocasionalmente podemos actualizar esta política o parte de ella, por lo que debemos revisarla periódicamente.
+            </p>
+
+            <p class="lp-paragrafo">
+                Liderpro Informática necesita de tu ayuda con comentarios, sugerencias y dudas sobre esta política o cualquier otra área de nuestro sitio web.
+            </p>
+        </div>
+    </div>
+</div>
