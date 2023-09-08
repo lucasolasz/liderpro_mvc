cdde916106ebd1e2a2454584e5ed5c26acbbe3e7
lucasolasz <olaszlucas072@gmail.com>
2023-09-08 19:44:02 -0300

    Traduz Lider Pro

---
+++ app/Views/lider_pro/lider_pro_en.php
@@ -0,0 +1,14 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <p class="lp-paragrafo mt-5">
+        LIDERPRO, over its more than 20 years, has organized a portfolio of services with multiple platforms and a variety of equipment that cover most of our customers' ICT (IT and Telephony) needs, providing better value for services and rapid execution. of projects.
+    </p>
+
+    <p class="lp-paragrafo">
+        We serve small to large companies in the cities of Rio de Janeiro and São Paulo, always focusing on service excellence and technical quality of service. We are located close to the city center of Rio de Janeiro and the Glória subway station, with our own parking, an anti-aesthetic laboratory - one of the only ones in the city - and the technical experience to provide services with maximum agility. </p>
+
+    <p class="lp-paragrafo">
+        We would like to show how responsible we feel so that the “computer” tool is increasingly useful to our customers, optimizing financial, time and human resources. Giving them faster service and internal and external communication within your company, placing it increasingly ahead in its segment. </p>
+</div>
---
+++ app/Views/lider_pro/lider_pro_es.php
@@ -0,0 +1,16 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <p class="lp-paragrafo mt-5">
+        LIDERPRO, a lo largo de sus más de 20 años, ha organizado un portafolio de servicios con múltiples plataformas y una variedad de equipos que cubren la mayoría de las necesidades TIC (TI y Telefonía) de nuestros clientes, brindando mayor valor por los servicios y rápida ejecución de proyectos.
+    </p>
+
+    <p class="lp-paragrafo">
+        Atendemos a pequeñas y grandes empresas en las ciudades de Río de Janeiro y São Paulo, siempre enfocadas en la excelencia en el servicio y la calidad técnica del servicio. Estamos ubicados cerca del centro de la ciudad de Río de Janeiro y de la estación de metro Glória, con estacionamiento propio, un laboratorio antiestético -uno de los únicos en la ciudad- y la experiencia técnica para brindar servicios con la máxima agilidad.
+    </p>
+
+    <p class="lp-paragrafo">
+        Nos gustaría mostrar cuánto nos sentimos responsables de hacer que la herramienta “informática” sea cada vez más útil para nuestros clientes, optimizando recursos económicos, de tiempo y humanos. Aportándoles más rapidez al servicio y a la comunicación interna y externa de su empresa, situándola cada vez más a la cabeza en su segmento de actividad.
+    </p>
+</div>
