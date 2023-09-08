4f8e72531722e1890c93bd9a1a2953818013a2b4
lucasolasz <olaszlucas072@gmail.com>
2023-09-08 19:52:31 -0300

    Traduz termos de uso

---
+++ app/Views/lider_pro/termos_de_uso_en.php
@@ -0,0 +1,64 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <div class="row p-5">
+        <div class="col-md-12 col-lg-12 d-flex  justify-content-center">
+            <p class="lp-paragrafo-sublinhado">TERMS OF USE</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-lg-12">
+            <p class="lp-paragrafo">
+                Please, it is important to read the rules of use prior to use.
+                <br>
+                This website was designed and built by Liderpro Informática EIRELI with the aim of serving its customers only and exclusively. All services and information contained in this website must be used according to the rules specified below. If the user does not agree with the rules, he must not access any of the areas of this website.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Copy Right </span>
+                <br>
+                All information contained in this website may be copied only for personal, internal, educational and/or non-commercial use. The information may not be altered, be part of other information belonging to the user or third parties, used in public or commercial events, transferred or transmitted to third parties and/or sold or rented. Except with the written permission of Liderpro Informática determining their use. This permission to copy is not a sale, lease or transfer of title.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Brazilian Laws </span>
+                <br>
+                This website was prepared based on the laws of Brazil. Liderpro Informática is not responsible for any disclosure of the content of its website in other countries. It is prohibited to insert or transmit any type of illegal material to this website. The content that is part of this website and the Liderpro Informática logo are protected by copyright, trademark, patent and any other law.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Website of Manufacturers or Companies (Third Parties) </span>
+                <br>
+                Liderpro Informática is not responsible for any problems that the indicated websites may cause with the download of their programs (software or drivers), with the content found there or any connection (link) that it points to. None of the information, products and/or services presented on the indicated websites are endorsed by Liderpro Informática. The entire risk of accessing the connections (links) indicated on this website is the responsibility of the user.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Trademarks </span>
+                <br>
+                All brands appearing on this website are registered trademarks of their respective owners. This website does not have, through the trademarks that appear, the objective of promoting products or advertising them.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Information sources </span>
+                <br>
+                The information that is not written by Liderpro Informática, will have, at the end of each text, its bibliography.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Change Content </span>
+                <br>
+                Liderpro Informática reserves the right to change the content of any part of its website or text written by it without prior notice.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Personal information </span>
+                <br>
+                All personal information (Individuals or Legal Entities) sent to Liderpro Informática through Electronic Forms are governed by the Privacy Policy.
+            </p>
+        </div>
+    </div>
+
+
+</div>
---
+++ app/Views/lider_pro/termos_de_uso_es.php
@@ -0,0 +1,64 @@
+<div class="container p-5">
+
+    <?php include APP . '/Views/lider_pro/menu_opcoes.php'; ?>
+
+    <div class="row p-5">
+        <div class="col-md-12 col-lg-12 d-flex  justify-content-center">
+            <p class="lp-paragrafo-sublinhado">TERMINOS DE USO</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-lg-12">
+            <p class="lp-paragrafo">
+                Por favor, es importante leer las normas de uso antes de utilizarlo.
+                <br>
+                Este sitio web fue diseñado y construido por Liderpro Informática EIRELI con el objetivo de servir única y exclusivamente a sus clientes. Todos los servicios e información contenidos en este sitio web deben utilizarse de acuerdo con los estándares especificados a continuación. Si el usuario no está de acuerdo con las normas no deberá acceder a ninguna de las áreas de este sitio web.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Copiar a la derecha </span>
+                <br>
+                Toda la información contenida en este sitio web puede copiarse únicamente para uso personal, interno, educativo y/o no comercial. La información no puede ser alterada, formar parte de otra información del usuario o de terceros, utilizarla en eventos públicos o comerciales, transferirla o transmitirla a terceros y/o venderla o alquilarla. Salvo autorización escrita de Liderpro Informática determinando su uso. Este permiso para copiar no es una venta, arrendamiento o transferencia de título.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Leyes brasileñas </span>
+                <br>
+                Este sitio web fue elaborado con base en las leyes de Brasil. Liderpro Informática no se hace responsable de la difusión del contenido de su sitio web en otros países. Está prohibido insertar o transmitir cualquier tipo de material ilegal a este sitio web. El contenido que forma parte de este sitio web y el logotipo de Liderpro Informática se encuentran protegidos por derechos de autor, marcas, patentes y cualquier otra ley.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Sitio Web de Fabricantes o Empresas (Terceros) </span>
+                <br>
+                Liderpro Informática no se hace responsable de los problemas que los sitios web indicados puedan ocasionar con la descarga de sus programas (software o drivers), con el contenido que allí se encuentre o cualquier conexión (enlace) a la que apunte. Ninguna información, productos y/o servicios presentados en los sitios web indicados están respaldados por Liderpro Informática. Todo el riesgo del acceso a las conexiones (links) indicados en este sitio web es responsabilidad del usuario.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Marcas registradas </span>
+                <br>
+                Todas las marcas que aparecen en este sitio web son marcas registradas de sus respectivos propietarios. Este sitio web no tiene, a través de las marcas que aparecen, el objetivo de promocionar productos o publicitarlos.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Fuentes de información </span>
+                <br>
+                La información que no esté escrita por Liderpro Informática, tendrá, al final de cada texto, su bibliografía.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Cambiar contenido </span>
+                <br>
+                Liderpro Informática se reserva el derecho de cambiar el contenido de cualquier parte de su sitio web o texto escrito por él sin previo aviso.
+            </p>
+
+            <p class="lp-paragrafo">
+                <span class="lp-titulo-paragrafo"> Informaciones personales </span>
+                <br>
+                Toda la información personal (Personas Físicas o Jurídicas) enviada a Liderpro Informática a través de Formularios Electrónicos se rige por la Política de Privacidad.
+            </p>
+        </div>
+    </div>
+
+
+</div>
