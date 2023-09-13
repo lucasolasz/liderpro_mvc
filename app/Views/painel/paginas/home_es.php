ca25928b0a2c9187873469a24cacf70cc571cac3
lucasolasz <olaszlucas072@gmail.com>
2023-09-13 00:11:40 -0300

    Traduz home

---
+++ app/Views/painel/paginas/home_es.php
@@ -0,0 +1,52 @@
+<div class="container py-3">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?= URL . '/img/banner/banner_gestao_informatica.png' ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= URL . '/img/banner/banner_infra_rede.png' ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= URL . '/img/banner/banner_nobreak.png' ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= URL . '/img/banner/banner_solucao_nuvem.png' ?>" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </button>
+    </div>
+
+
+    <div class="row py-5 lp-home-gifs">
+        <div class="col-md">
+            <h6>NUESTROS NÚMEROS</h6>
+            <img class="w-100 p-3" src="<?= URL . '/img/gifs_home/nossos_numeros.gif' ?>" alt="">
+        </div>
+        <div class="col-md">
+            <h6>ASOCIACIONES Y CERTIFICACIONES</h6>
+            <img class="w-100 p-3" src="<?= URL . '/img/gifs_home/parcerias.gif' ?>" alt="">
+        </div>
+        <div class="col-md">
+            <h6>SERVICIOS ESPECIALES</h6>
+            <img class="w-100 p-3" src="<?= URL . '/img/gifs_home/servicos_especiais.gif' ?>" alt="">
+        </div>
+        <div class="col-md">
+            <h6>DEPOSICIONES</h6>
+            <img class="w-100 p-3" src="<?= URL . '/img/gifs_home/depoimentos.gif' ?>" alt="">
+        </div>
+    </div>
+</div>
